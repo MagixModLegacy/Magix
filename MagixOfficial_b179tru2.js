a7aa14303235264f4d68ba1df6c347fe69d3ef97
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-01-19 15:06:44 +0100

    Update and rename MagixOfficial_b179tru1.js to MagixOfficial_b179tru2.js

--- MagixOfficial_b179tru2.js
+++ MagixOfficial_b179tru2.js
@@ -4769,7 +4769,7 @@ func:function(){
 		G.getDict('mine').effects.push({type:'gather',context:'mine',what:{'Diorite':30},max:25,mode:'ostones'});
 		G.getDict('mine').effects.push({type:'gather',context:'mine',what:{'Andesite':30},max:25,mode:'ostones'});
 	//upkeep test
-		G.getDict('furnace').upkeep.push({'log':1});
+		G.getDict('furnace').upkeep:{'log':1};
 //2 modes for architect
 		G.getDict('architect').modes['Brickhouser']={
 			name:'Brickhouse building',
