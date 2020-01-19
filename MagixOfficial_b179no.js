334f8c1fdeb68091af58598056f400f1cef678d0
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-01-19 20:22:38 +0100

    Update and rename MagixOfficial_b179nn.js to MagixOfficial_b179no.js

--- MagixOfficial_b179no.js
+++ MagixOfficial_b179no.js
@@ -2416,17 +2416,20 @@ let gift =     new G.Tech({
     });
 function checkMagic(){
   if(G.achiev[0].won){
+      if(!G.has(gift)){
     G.gainTech(gift)
     G.Message({
       type:"good",
       text: "Since you have built the Mausoleum it the past, you have access to magic!",
-	 icon:[4,12,6,1,'magixmod']
+	icon:[4,12,6,1,'magixmod']
     })
+
+}
   } else {
   G.Message({
       type:"bad",
       text: "Since you haven't built the Mausoleum it the past yet, you don't have access to magic yet",
-	icon:[3,12,6,1,'magixmod']
+	  icon:[3,12,6,1,'magixmod']
    })
   }
 }
