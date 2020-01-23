afbb54dfba107ff37cff7c923970038a9b084d22
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-01-23 18:50:07 +0100

    Update and rename MagixOfficial_b180b.js to MagixOfficial_b180c.js

--- MagixOfficial_b180c.js
+++ MagixOfficial_b180c.js
@@ -3038,8 +3038,8 @@ G.NewGameConfirm = new Proxy(oldNewGame, {
 		req:{'Flour-crafting':true},
 	});
 let thieffight = new G.Tech({
-    name:{'Battling thieves'},
-    desc:{'Bad news... committed a crime... It is time to fight against [thief,thieves] . @Allows you to hire a [Thief hunter] .'},
+    name:'Battling thieves',
+    desc:'Bad news... committed a crime... It is time to fight against [thief,thieves] . @Allows you to hire a [Thief hunter] .',
     icon:[22, 16, "magixmod"],
     cost:{'insight':90},
     req:{'tribalism':false,'hunting':true}
