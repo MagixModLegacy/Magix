560b406dc7bf5976b84fbebf54d596668655fe3d
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-01-23 18:00:34 +0100

    Update and rename MagixOfficial_b179tpj8.js to MagixOfficial_b180.js

--- MagixOfficial_b180.js
+++ MagixOfficial_b180.js
@@ -3042,7 +3042,7 @@ let thieffight = new G.Tech({
     desc:
         "Bad news... committed a crime... It is time to fight against [thief,thieves] . @Allows you to hire a [Thief hunter] .",
     icon: [22, 16, "magixmod"],
-    cost: {'insight':100},
+    cost: {'insight':90},
     req: {'tribalism':false,'hunting':true}
 })
 function thieffightS() {
