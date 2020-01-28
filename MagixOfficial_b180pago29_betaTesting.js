cd5f58cc092116acf626d8db07c82e1ae0c0842b
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-01-28 05:56:06 +0100

    Update and rename MagixOfficial_b180pago28_betaTesting.js to MagixOfficial_b180pago29_betaTesting.js

--- MagixOfficial_b180pago29_betaTesting.js
+++ MagixOfficial_b180pago29_betaTesting.js
@@ -3046,7 +3046,8 @@ G.NewGameConfirm = new Proxy(oldNewGame, {
 		effects:[]//manual unlocking blocker
 	});
 function autobuy(newBuy) {
-  if(G.hasNot(battlingThieves) && newBuy >= 8) G.gainTech(battlingThieves)
+  if(G.hasNot('Battling Thieves') && newBuy >= 8);
+	G.gainTech(battlingThieves);
 }
 G = new Proxy(G, {
   set: (src, prop, value) => {
