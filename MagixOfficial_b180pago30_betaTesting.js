411231b8cb3ba8b74f81916e8ecb3f53e1505413
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-01-28 06:00:08 +0100

    Update MagixOfficial_b180pago30_betaTesting.js

--- MagixOfficial_b180pago30_betaTesting.js
+++ MagixOfficial_b180pago30_betaTesting.js
@@ -3046,8 +3046,7 @@ G.NewGameConfirm = new Proxy(oldNewGame, {
 		effects:[]//manual unlocking blocker
 	});
 function autobuy(newBuy) {
-  if(G.hasNot('Battling thieves') && newBuy >= 8);
-	G.gainTech(battlingThieves);
+  if(G.hasNot('Battling thieves') && newBuy >= 8) G.gainTech(battlingThieves)
 }
 G = new Proxy(G, {
   set: (src, prop, value) => {
