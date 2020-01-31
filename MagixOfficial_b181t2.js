a6637aa0736ae8679e2ea084550c622e1131ebfb
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-01-31 18:50:35 +0100

    Update and rename MagixOfficial_b181.js to MagixOfficial_b181t2.js

--- MagixOfficial_b181t2.js
+++ MagixOfficial_b181t2.js
@@ -743,14 +743,7 @@ func:function(){
   		 tick:function(me,tick) //Making wonders having their effects.
        		 {
            	 if (G.has('Knowledgeable') && (G.getRes('insight').amount < G.getRes('wisdom').amount));
-           	 G.gain('insight', (G.getRes('wisdom').amount - G.getRes('insight').amount) > 0.75 ? 0.75 : (G.getRes('wisdom').amount - G.getRes('insight').amount) ,'Complex of Dreamers');
-		
-		if (G.has('Cultural forces arise') && (G.getRes('culture').amount < G.getRes('inspiration').amount));
-           	 G.gain('culture', (G.getRes('inspiration').amount - G.getRes('culture').amount) > 0.75 ? 0.75 : (G.getRes('inspiration').amount - G.getRes('culture').amount) ,'Fortress of cultural legacy');
-		
-		if (G.has('Politic power rising up') && (G.getRes('influence').amount < G.getRes('authority').amount));
-		    G.gain('influence', (G.getRes('authority').amount - G.getRes('influence').amount) > 0.75 ? 0.75 : (G.getRes('authority').amount - G.getRes('influence').amount) ,'Pagoda of democracy');
-            
+           	 G.gain('insight', (G.getRes('wisdom').amount - G.getRes('insight').amount) > 0.75 ? 0.75 : (G.getRes('wisdom').amount - G.getRes('insight').amount) ,'Complex of Dreamers'); 
 	},
 		category:'misc',
 	});
@@ -1064,6 +1057,11 @@ func:function(){
 		desc:'The bigger limit the more essence.',
 		icon:[0,2,'magixmod'],
 		hidden:true,
+		tick:function(me,tick)
+			{
+			if (G.has('Cultural forces arise') && (G.getRes('culture').amount < G.getRes('inspiration').amount));
+            		G.gain('culture', (G.getRes('inspiration').amount - G.getRes('culture').amount) > 0.75 ? 0.75 : (G.getRes('inspiration').amount - G.getRes('culture').amount) ,'Fortress of cultural legacy');
+		},
 		category:'main',
 	});
 		new G.Res({
@@ -1071,6 +1069,11 @@ func:function(){
 		desc:'The bigger limit the more essence.',
 		icon:[0,1,'magixmod'],
 		category:'main',
+		tick:function(me,tick)
+			{
+			if (G.has('Cultural forces arise') && (G.getRes('culture').amount < G.getRes('inspiration').amount));
+            		G.gain('culture', (G.getRes('inspiration').amount - G.getRes('culture').amount) > 0.75 ? 0.75 : (G.getRes('inspiration').amount - G.getRes('culture').amount) ,'Fortress of cultural legacy');
+		},
 		hidden:true,
 	});
 		new G.Res({
@@ -1078,6 +1081,10 @@ func:function(){
 		desc:'The bigger limit the more essence.',
 		icon:[0,3,'magixmod'],
 		category:'main',
+		tick:function(me,tick)
+			if (G.has('Politic power rising up') && (G.getRes('influence').amount < G.getRes('authority').amount));
+            		G.gain('influence', (G.getRes('authority').amount - G.getRes('influence').amount) > 0.75 ? 0.75 : (G.getRes('authority').amount - G.getRes('influence').amount) ,'Pagoda of democracy');
+		},
 		hidden:true,
 	});
 		new G.Res({
