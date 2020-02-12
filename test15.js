0834c9a5e37122d99975e7a87f6a9a04e43d0fba
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-02-12 20:10:27 +0100

    Update and rename test14.js to test15.js

--- test15.js
+++ test15.js
@@ -6542,7 +6542,7 @@ new G.Unit({
 	new G.ChooseBox({
 		name:'research box',
 		context:'tech',
-		choicesN:10,
+		choicesN:4,
 		getCosts:function()
 		{
 			var cost=Math.floor(G.getRes('wisdom').amount*(0.025+0.05*this.roll));
