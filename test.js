92671eded687ae7e434946cef427f5df278e1a8a
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-02-12 11:46:32 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+G.AddData({
+name:'Magix',
+author:'pelletsstarPL',
+desc:'Magic! Magic!. Fit more people, discover essences which have its secret use. At the moment you can reach new dimensions which will increase your max land soon. More housing so you can fit more people. @Mod utilizes vanilla part of the game by adding new modes or new units.',
+engineVersion:1,
+manifest:'ModManifest.js',
+requires:['Default dataset*'],
+sheets:{'magixmod':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/magixmod.png','seasonal':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/seasonalMagix.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+func:function(){
+//READ THIS: All rights reserved to mod creator and people that were helping the main creator with coding. Mod creator rejects law to copying icons from icon sheets used for this mod. All noticed plagiariasm will be punished. Copyright: 2020 
