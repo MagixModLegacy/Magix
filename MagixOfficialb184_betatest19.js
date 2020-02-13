9d84de221a203ac8e396656bae199360e88b8224
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-02-13 11:54:45 +0100

    Update and rename MagixOfficialb184_betatest18.js to MagixOfficialb184_betatest19.js

--- MagixOfficialb184_betatest19.js
+++ MagixOfficialb184_betatest19.js
@@ -485,6 +485,7 @@ G.writeMSettingButton=function(obj)
 						}
 					}
 				}
+			}
 				
 				
 				var productionMult=G.doFunc('production multiplier',1);
@@ -2972,125 +2973,6 @@ G.writeMSettingButton=function(obj)
 		meta:true,
 		tick:function(me,tick)
 		{	
-				var productionMult=G.doFunc('production multiplier',1);
-				
-				var deathUnhappinessMult=1;
-				if (G.has('fear of death')) deathUnhappinessMult*=2;
-				if (G.has('belief in the afterlife')) deathUnhappinessMult/=2;
-				if (G.has('Hope of revenant abandoning')) deathUnhappinessMult/=2;
-				if (tick%3==0 && G.checkPolicy('disable eating')=='off')
-				{
-					//drink water
-					var toConsume=0;
-					var weights={'Child alchemist':0.3,'Alchemist':0.5,'Instructor':0.5,'drunk':0.4};
-					for (var i in weights)
-					{toConsume+=G.getRes(i).amount*weights[i];}
-					var rations=G.checkPolicy('water rations');
-					if (rations=='none') {toConsume=0;G.gain('happiness',-me.amount*3,'water rations');G.gain('health',-me.amount*2,'water rations');}
-					else if (rations=='meager') {toConsume*=0.5;G.gain('happiness',-me.amount*1,'water rations');G.gain('health',-me.amount*0.5,'water rations')}
-					else if (rations=='plentiful') {toConsume*=1.5;G.gain('happiness',me.amount*1,'water rations');}
-					toConsume=randomFloor(toConsume);
-					var lacking=toConsume-G.lose('water',toConsume,'drinking');
-					if (rations=='none') lacking=me.amount*0.5;
-					if (lacking>0)//are we out of water?
-					{
-						//resort to muddy water
-						if (rations!='none' && G.checkPolicy('drink muddy water')=='on') lacking=lacking-G.lose('muddy water',lacking,'drinking');
-						//resort to cloudy water
-						if (rations!='none' && G.checkPolicy('drink cloudy water')=='on') lacking=lacking-G.lose('Cloudy water',lacking,'drinking');
-						if (lacking>0 && G.checkPolicy('disable aging')=='off')//are we also out of muddy water?
-						{
-							G.gain('happiness',-lacking*5,'no water');
-							//die off
-							var toDie=(lacking/5)*0.05;
-							if (G.year<1) toDie/=5;//less deaths in the first year
-							var died=0;
-							var weights={'Child alchemist':0.2,'Alchemist':0.5,'Instructor':0.5};//the elderly are the first to starve off
-							var sum=0;for (var i in weights){sum+=weights[i];}for (var i in weights){weights[i]/=sum;}//normalize
-							for (var i in weights){var ratio=(G.getRes(i).amount/me.amount);weights[i]=ratio+(1-ratio)*weights[i];}
-							for (var i in weights)
-							{var n=G.lose(i,randomFloor((Math.random()*0.8+0.2)*toDie*weights[i]),'dehydration');died+=n;}
-							G.gain('corpse',died,'dehydration');
-							G.gain('happiness',-died*20*deathUnhappinessMult,'dehydration');
-							G.getRes('died this year').amount+=died;
-							if (died>0) G.Message({type:'bad',mergeId:'diedDehydration',textFunc:function(args){return B(args.died)+' '+(args.died==1?'person':'people')+' died from dehydration.';},args:{died:died},icon:[5,4]});
-						}
-					}
-					
-					//eat food
-					var toConsume=0;
-					var consumeMult=1;
-					var happinessAdd=0;
-					if (G.has('culture of moderation')) {consumeMult*=0.85;happinessAdd-=0.1;}
-					else if (G.has('joy of eating')) {consumeMult*=1.15;happinessAdd+=0.1;}
-					var weights={'Child alchemist':0.5,'Alchemist':1,'Instructor':1};
-					for (var i in weights)
-					{toConsume+=G.getRes(i).amount*weights[i];}
-					var rations=G.checkPolicy('food rations');
-					if (rations=='none') {toConsume=0;G.gain('happiness',-me.amount*3,'food rations');G.gain('health',-me.amount*2,'food rations');}
-					else if (rations=='meager') {toConsume*=0.5;G.gain('happiness',-me.amount*1,'food rations');G.gain('health',-me.amount*0.5,'food rations');}
-					else if (rations=='plentiful') {toConsume*=1.5;G.gain('happiness',me.amount*1,'food rations');}
-					toConsume=randomFloor(toConsume*consumeMult);
-					var consumed=G.lose('food',toConsume,'eating');
-					G.gain('happiness',G.lose('salt',randomFloor(consumed*0.1),'eating')*5,'salting food');//use salt
-					G.gain('happiness',consumed*happinessAdd,'food culture');
-					var lacking=toConsume-consumed;
-					if (rations=='none') lacking=me.amount*1;
-					
-					if (lacking>0)//are we out of food?
-					{
-						//resort to spoiled food
-						if (rations!='none' && G.checkPolicy('eat spoiled food')=='on') lacking=lacking-G.lose('spoiled food',lacking,'eating');
-						if (lacking>0 && G.checkPolicy('disable aging')=='off')//are we also out of spoiled food?
-						{
-							G.gain('happiness',-lacking*5,'no food');
-							//die off
-							var toDie=(lacking/5)*0.05;
-							if (G.year<1) toDie/=5;//less deaths in the first year
-							var died=0;
-							var weights={'Child alchemist':0.2,'Alchemist':0.5,'Instructor':0.5,'drunk':0.4};//the elderly are the first to starve off
-							var sum=0;for (var i in weights){sum+=weights[i];}for (var i in weights){weights[i]/=sum;}//normalize
-							for (var i in weights){var ratio=(G.getRes(i).amount/me.amount);weights[i]=ratio+(1-ratio)*weights[i];}
-							for (var i in weights)
-							{var n=G.lose(i,randomFloor((Math.random()*0.8+0.2)*toDie*weights[i]),'starvation');died+=n;}
-							G.gain('corpse',died,'starvation');
-							G.gain('happiness',-died*20*deathUnhappinessMult,'starvation');
-							G.getRes('died this year').amount+=died;
-							if (died>0) G.Message({type:'bad',mergeId:'diedStarvation',textFunc:function(args){return B(args.died)+' '+(args.died==1?'person':'people')+' died from starvation.';},args:{died:died},icon:[5,4]});
-						}
-					}
-				}
-				
-				//clothing
-				var objects={'basic clothes':[0.1,0.1],'primitive clothes':[0,0],'Colored clothing':[0.1,0.1]};
-				var leftout=me.amount;
-				var prev=leftout;
-				var fulfilled=0;
-				for (var i in objects)
-				{
-					fulfilled=Math.min(me.amount,Math.min(G.getRes(i).amount,leftout));
-					G.gain('happiness',fulfilled*objects[i][0],'clothing');
-					G.gain('health',fulfilled*objects[i][1],'clothing');
-					leftout-=fulfilled;
-				}
-				G.gain('happiness',-leftout*0.15,'no clothing');
-				G.gain('health',-leftout*0.15,'no clothing');
-				
-				//fire
-				var objects={'fire pit':[10,0.1,0.1]};
-				var leftout=me.amount;
-				var prev=leftout;
-				var fulfilled=0;
-				for (var i in objects)
-				{
-					fulfilled=Math.min(me.amount,Math.min(G.getRes(i).amount*objects[i][0],leftout));
-					G.gain('happiness',fulfilled*objects[i][1],'warmth & light');
-					G.gain('health',fulfilled*objects[i][2],'warmth & light');
-					leftout-=fulfilled;
-				}
-				G.gain('happiness',-leftout*0.1,'cold & darkness');
-				G.gain('health',-leftout*0.1,'cold & darkness');
-			}
 		}
 	});
 		new G.Res({
