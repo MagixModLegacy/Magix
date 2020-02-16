29d4fda189866ef2456a60b60b35b81a63b25f90
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-02-16 11:48:03 +0100

    Update and rename MagixOfficialb184_betatest35.js to MagixOfficialb184_betatest36.js

--- MagixOfficialb184_betatest36.js
+++ MagixOfficialb184_betatest36.js
@@ -3117,19 +3117,19 @@ G.writeMSettingButton=function(obj)
 				u1popup = true
 				}
 				if (G.year>=11 && G.year<=15 && !u2popup){
-       				 G.Message({text:'People continue digging down and another souls want to tell you few things and green soul seemed like Nature essence creation has bitten you. People complain for warmth.<b><font color="fuschia">The world you want to meet</br>Will not give all it has right before your feet</br>Danger for people abounds</br>and forbidden will become crowds</font></b>',icon:[1,19,'magixmod']});
+       				 G.Message({text:'People continue digging down and another souls want to tell you few things and green soul seemed like Nature essence creation has bitten you. People complain for warmth.</br><b><font color="fuschia">The world you want to meet</br>Will not give all it has right before your feet</br>Danger for people abounds</br>and forbidden will become crowds</font></b>',icon:[1,19,'magixmod']});
 				u2popup = true
 				}
 				if (G.year>=12 && G.year<=16 && !u3popup){
-       				 G.Message({text:'You see wizards using magic to cool down the warmth so people can continue digging down for new world. Dark voices yells and makes feared civils near you. This doesn\'t seem well.<b><font color="teal">You want to see fear...</br>Please don\'t show me even one your tear...</br>You hear...</br>I yell so you are fear.</font></b>',icon:[2,19,'magixmod']});
+       				 G.Message({text:'You see wizards using magic to cool down the warmth so people can continue digging down for new world. Dark voices yells and makes feared civils near you. This doesn\'t seem well.</br><b><font color="teal">You want to see fear...</br>Please don\'t show me even one your tear...</br>You hear...</br>I yell so you are fear.</font></b>',icon:[2,19,'magixmod']});
 				u3popup = true
 				}
 				if (G.year>=13 && G.year<=17 && !u4popup){
-       				 G.Message({text:'Out of nowhere people yell and run away in panic except small group of braves who still dig down. Souls start behave weird... or you just don\'t understand them yet<b><font color="red">Don\'t let the Gods</br>Send there his crowds</br>Danger for people abounds</br>and forbidden are becoming crowds</font></b>',icon:[3,19,'magixmod']});
+       				 G.Message({text:'Out of nowhere people yell and run away in panic except small group of braves who still dig down. Souls start behave weird... or you just don\'t understand them yet</br><b><font color="red">Don\'t let the Gods</br>Send there his crowds</br>Danger for people abounds</br>and forbidden are becoming crowds</font></b>',icon:[3,19,'magixmod']});
 				u4popup = true
 				}
 				if (G.year>=14 && G.year<=18 && !u5popup){
-       				 G.Message({text:'A huge cavern starts to show while braves continued digging down. They run away to you... empopwered weirdly by these souls. Another lightning essence creature paralyses you and water essence creatures started behaving insane. Is this greeting from new world?<b><font color="silver">Alright '+G.getName('ruler')+' ...</br>Call your soul and make it go...</br>Right to the world</br>The Under...World</font></b>',icon:[4,19,'magixmod']});
+       				 G.Message({text:'A huge cavern starts to show while braves continued digging down. They run away to you... empopwered weirdly by these souls. Another lightning essence creature paralyses you and water essence creatures started behaving insane. Is this greeting from new world?</br><b><font color="silver">Alright '+G.getName('ruler')+' ...</br>Call your soul and make it go...</br>Right to the world</br>The Under...World</font></b>',icon:[4,19,'magixmod']});
 				u5popup = true
 				}
 		},
