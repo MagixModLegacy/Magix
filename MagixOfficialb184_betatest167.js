fb390d618f27d29274931810272025d42112268f
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-02-19 15:36:47 +0100

    Update and rename MagixOfficialb184_betatest166.js to MagixOfficialb184_betatest167.js

--- MagixOfficialb184_betatest167.js
+++ MagixOfficialb184_betatest167.js
@@ -6769,7 +6769,10 @@ new G.Unit({
 		},
 		buttonTooltip:function()
 		{
+			if (G.hasNot('Eotm'))
 			return '<div class="info"><div class="par">'+(this.choices.length==0?'Generate new research opportunities.<br>The cost scales with your Wisdom resource.':'Reroll into new research opportunities if none of the available choices suit you.<br>Cost increases with each reroll, but will decrease again over time.')+'</div><div>Cost : '+G.getCostString(this.getCosts(),true)+'.</div></div>';
+		}else{
+			return '<div class="info"><div class="par">'+(this.choices.length==0?'Generate new research opportunities.<br>The cost scales with your Wisdom II resource.':'Reroll into new research opportunities if none of the available choices suit you.<br>Cost increases with each reroll, but will decrease again over time.')+'</div><div>Cost : '+G.getCostString(this.getCosts(),true)+'.</div></div>';
 		}
 	});
 	
