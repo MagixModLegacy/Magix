08f05f76f0ea6eda90ac5612e2e0d1d39a1eaf22
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-02-20 07:25:10 +0100

    Update and rename MagixOfficialb184_betatest193.js to MagixOfficialb184_betatest194.js

--- MagixOfficialb184_betatest194.js
+++ MagixOfficialb184_betatest194.js
@@ -9148,13 +9148,6 @@ autobuy(G.year)
 		category:'seasonal',
 		hidden:true,
 	});
-		new G.Tech({
-		name:'Test',
-		desc:'@[Artisan of new year] now can craft [Dark Orange Firework] and [Dark Blue Firework].',
-		icon:[16,0,'seasonal'],
-		cost:{'insight II':35,'culture II':5},
-		req:{'Eotm':true},
-	});
 	/*=====================================================================================
 	LANDS
 	=======================================================================================*/
@@ -10094,6 +10087,13 @@ G.NewGameConfirm = new Proxy(oldNewGame5, {
     checkUnA()
   }
 })
+			new G.Tech({
+		name:'Test',
+		desc:'@[Artisan of new year] now can craft [Dark Orange Firework] and [Dark Blue Firework].',
+		icon:[16,0,'seasonal'],
+		cost:{'insight II':35,'culture II':5},
+		req:{'Eotm':true},
+	});
 	/*=====================================================================================
 	MAP GENERATOR
 	=======================================================================================*/
