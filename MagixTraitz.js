c2711ad7c41fbb342a2b2ee7bb8c956a994bb91a
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-02-21 12:02:29 +0100

    Update MagixTraitz.js

--- MagixTraitz.js
+++ MagixTraitz.js
@@ -1,4 +1,4 @@
-	new G.Trait({
+		new G.Trait({
 		name:'Belief in portals',
 		desc:'@Makes wizards attempting to create a new dimension',
 		icon:[2,1,'magixmod'],
@@ -20,7 +20,7 @@
 		desc:'<span style="color: #aaffff">After few years since you started crafting [Juices] you noticed your people make most <b>tasty juice<b> ever you drank. Since gaining this trait you\'ll get these bonuses: @Happiness caused by drinking juices boosted by 25%. @Health given by drinking juices boosted by 25%. @Due to these bonuses [Juices] will now need little bit more ingredients to craft. @[artisan of juice] has a small chance to craft 1 additional [Juices,juice]. </span>',
 		icon:[16,5,'magixmod'],
 		cost:{'Juices':6.5e3,'wisdom':25,'insight':30},
-		chance:6,//experimental
+		chance:6,
 		category:'knowledge',
 		req:{'Crafting a juice':true},
 	});
@@ -29,7 +29,7 @@
 		desc:'Your dreamers were thinking once how to make eating more healthy. Then they share its thoughts. Surprisingly they were right. People got healthier, feel better. @This trait makes [healer] generate health. People won\'t eat even more food so do not worry.',
 		icon:[16,7,'magixmod'],
 		cost:{'culture':150,'wisdom':25,'insight':100,'influence':10},
-		chance:120,//experimental
+		chance:120,
 		req:{'joy of eating':true,'Crafting a juice':true},
 	});
 		new G.Trait({
@@ -57,10 +57,6 @@
 		cost:{'insight':50,'culture':200,'inspiration':20,'authority':20,'spirituality':30,'faith':40},
 		chance:500,
 		req:{'belief in the afterlife':true},
-		tick:function(me,tick)
-		{
-			if (G.has('culture of the afterlife')) deathUnhappinessMult/=2;
-		},
 	});
 		new G.Trait({
 		name:'The God\'s call',
@@ -183,6 +179,24 @@
 		req:{'The God\'s call':true,'7th essence':true},
 		category:'gods',
 	});
+		new G.Trait({
+		name:'God\'s trait #4 Potter\'s frenzy',
+		desc:'Increases efficiency of [Factory of pots] and [Hut of potters] by 25% without harming people\'s [happiness].',
+		icon:[21,18,'magixmod'],
+		cost:{'faith':100},
+		chance:275,
+		req:{'The God\'s call':true,'7th essence':true},
+		category:'gods',
+	});
+		new G.Trait({
+		name:'God\'s trait #5 Colored life',
+		desc:'Increases efficiency of [Hovel of colours] by 25% without harming people\'s [happiness].',
+		icon:[22,18,'magixmod'],
+		cost:{'faith':100},
+		chance:275,
+		req:{'The God\'s call':true,'7th essence':true,'<font color="maroon">Caretaking</font>':true},
+		category:'gods',
+	});
 	//Moderation or caretaking?
 		new G.Trait({
 		name:'<font color="maroon">Moderation</font>',
@@ -201,4 +215,277 @@
 		req:{'joy of eating':true,'<font color="maroon">Moderation</font>':false},
 		chance:1000,
 		category:'main',
+	});
+	//Another knowledge
+		new G.Trait({
+		name:'Measuring system',
+		desc:'<span style="color: #aaffff">People noticed that they will need a measuring system to make constructing, planning easier... so they created their own system of measuring things.</span>.',
+		icon:[13,18,'magixmod'],
+		cost:{'wisdom':75},
+		chance:475,
+		req:{'Will to know more':true},
+		category:'knowledge',
+	});
+		new G.Trait({
+		name:'An opposite side of belief',
+		desc:'Religious people are repeating that there a opposite of the God they have encountered in Paradise exists.',
+		icon:[11,19,'magixmod'],
+		cost:{'spirituality':10,'faith':200},
+		chance:375,
+		req:{'Paradise crafting':true}
+	});
+	//Devil 's traits
+		new G.Trait({
+		name:'dt1',
+		displayName:'Devil\'s trait #1 Lazy blacksmiths',
+		desc:'Rates of [blacksmith workshop]s production decreased by 5% at each mode. It involves its paradise version too.',
+		icon:[26,1,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt2':false,'dt3':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt2',
+		displayName:'Devil\'s trait #2 Lazy firekeepers',
+		desc:'Rates of [firekeeper]s production decreased by 3% at each mode',
+		icon:[26,2,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt1':false,'dt3':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+		new G.Trait({
+		name:'dt3',
+		displayName:'Devil\'s trait #3 Lazy carvers',
+		desc:'Rates of [carver]s production decreased by 5% at each mode.',
+		icon:[26,3,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt1':false,'dt2':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+		new G.Trait({
+		name:'dt4',
+		displayName:'Devil\'s trait #4 Precious material unluck',
+		desc:'All gathering of [gold ore,gold] and [platinum ore,platinum] decreased by 5%',
+		icon:[26,4,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt5':false,'dt6':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt5',
+		displayName:'Devil\'s trait #5 Hard material decay',
+		desc:'All gathering of resources that are used to craft [hard metal ingot]s decreased by 5%',
+		icon:[26,5,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt4':false,'dt6':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt6',
+		displayName:'Devil\'s trait #6 Soft metal decay',
+		desc:'All gathering of resources that are used to craft [soft metal ingot]s decreased by 5%',
+		icon:[26,6,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt4':false,'dt5':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+		new G.Trait({
+		name:'dt7',
+		displayName:'Devil\'s trait #7 Holy well drought',
+		desc:'[holy well] gathers 10% less [Cloudy water] than usual.',
+		icon:[26,7,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt8':false,'dt9':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+		new G.Trait({
+		name:'dt8',
+		displayName:'Devil\'s trait #8 Plain Island wells drought',
+		desc:'[well of the Plain Island] gathers 15% less [water] than usual.',
+		icon:[26,8,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt7':false,'dt9':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+		new G.Trait({
+		name:'dt9',
+		displayName:'Devil\'s trait #9 Nothing',
+		desc:'Damn... you got lucky. This devil\'s trait doesn\'t involve at any way and doesn\'t weaken anything.',
+		icon:[26,9,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt7':false,'dt8':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt10',
+		displayName:'Devil\'s trait #10 Clothing!!!',
+		desc:'Unhappiness from lack of clothing is doubled.',
+		icon:[26,10,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt11':false,'dt12':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt11',
+		displayName:'Devil\'s trait #11 Will of warmth',
+		desc:'Unhappiness from cold & darkness is doubled.',
+		icon:[26,11,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt10':false,'dt12':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt12',
+		displayName:'Devil\'s trait #12 Bury!!!',
+		desc:'Unhappiness from unburied corpses increased by 50%.',
+		icon:[26,12,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt10':false,'dt11':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt13',
+		displayName:'Devil\'s trait #13 Faith sapping',
+		desc:'Wipes away 0.5% of current [faith] amount.',
+		icon:[26,13,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt14':false,'dt15':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt14',
+		displayName:'Devil\'s trait #14 Influence sapping',
+		desc:'Wipes away 0.5% of current [influence] amount.',
+		icon:[26,14,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt13':false,'dt15':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt15',
+		displayName:'Devil\'s trait #15 Culture sapping',
+		desc:'Wipes away 0.5% of current [culture] amount.',
+		icon:[26,15,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt13':false,'dt14':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt16',
+		displayName:'Devil\'s trait #16 Worse soothsaying',
+		desc:'Soothsayer gains 33% less [faith].',
+		icon:[26,16,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt13':false,'dt14':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt17',
+		displayName:'Devil\'s trait #17 Uncuttable tree',
+		desc:'[woodcutter] gains 20% less [log,wood].',
+		icon:[26,17,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt13':false,'dt14':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+			new G.Trait({
+		name:'dt18',
+		displayName:'Devil\'s trait #18 Insight blindness',
+		desc:'[dreamer]s gather 33% less [insight].',
+		icon:[26,18,'magixmod'],
+		cost:{'culture':100},
+		chance:150,
+		req:{'Underworld building 1/2':true,'dt13':false,'dt14':false},
+		effects:[
+			{type:'provide res',what:{'New world point':1}},
+		],
+		category:'devils'
+	});
+		new G.Trait({
+		name:'Eotm',
+		displayName:'Evolution of the minds',
+		desc:'Replaces [insight], [culture], [faith] and [influence] with: [insight II],[culture II], [faith II] and [influence II] . @To obtain them you will unlock special unit that will convert each for instance 500 [insight] into 1 [insight II] point. In addition [storyteller] , [dreamer] , [chieftain] and [clan leader] work 90% less efficient becuase this evolution is like disaster for them all. @Since now choose box in <b>Research tab</b> will require [insight II] & [science] instead of [insight] .@So you will still need [Wizard]s and units you used to gather lower essentials. @Lower essentials has been hidden but remember... don\'t get rid of wizards.',
+		icon:[25,19,'magixmod'],
+		cost:{'culture':1000,'insight':1000,'influence':300,'faith':300},
+		chance:190,
+		req:{'Underworld building 2/2':true},
+		effects:[
+			{type:'show res',what:['insight II']},
+			{type:'show res',what:['culture II']},
+			{type:'show res',what:['faith II']},
+			{type:'show res',what:['influence II']},
+			{type:'provide res',what:{'wisdom II':50}},
+			{type:'provide res',what:{'inspiration II':30}},
+			{type:'provide res',what:{'spirituality II':5}},
+			{type:'provide res',what:{'authority II':5}},
+		],
+		category:'main'
 	});
