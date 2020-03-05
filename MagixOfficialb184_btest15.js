08401c92f0205b2deaddf5d2ff3c6db3bab563e0
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-03-05 17:02:55 +0100

    Update and rename MagixOfficialb184_btest14.js to MagixOfficialb184_btest15.js

--- MagixOfficialb184_btest15.js
+++ MagixOfficialb184_btest15.js
@@ -8213,7 +8213,7 @@ autobuy(G.year)
 		name:'Sewing III',
 		desc:'Upgrades sewing skills of your civilization. @[Clothier]s can craft [hardened clothes] while they learned how to weave better, longer-durable clothing.',
 		icon:[27,12,'magixmod'], 
-		cost:{'insight II':20,'insight':15},
+		cost:{'insight II':20,'insight':20},
 		req:{'Wizardry':true,'Sewing II':true,'weaving II':true,'Eotm':true},
 	});
 	
