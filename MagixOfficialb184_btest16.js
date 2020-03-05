ac045c1cb8ddd2200694fd4e47db1c8ecab53b24
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-03-05 17:56:29 +0100

    Update and rename MagixOfficialb184_btest15.js to MagixOfficialb184_btest16.js

--- MagixOfficialb184_btest16.js
+++ MagixOfficialb184_btest16.js
@@ -3894,6 +3894,9 @@ G.writeMSettingButton=function(obj)
 			}
 			if(G.has('weaving II'))
 					G.getDict('clothier').icon = [27,11,'magixmod'];
+			if(G.has('Factories I'))
+					G.getDict('Drying rack').category = '';
+					G.getDict('potter').category = '';
 		},
 		getDisplayAmount:researchGetDisplayAmount,
 		whenGathered:researchWhenGathered,
