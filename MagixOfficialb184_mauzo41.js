959696b1ed300a4ac218618b6d3a5245db8bc654
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-03-10 12:44:27 +0100

    Update and rename MagixOfficialb184_mauzo40.js to MagixOfficialb184_mauzo41.js

--- MagixOfficialb184_mauzo41.js
+++ MagixOfficialb184_mauzo41.js
@@ -10481,7 +10481,6 @@ G.NewGameConfirm = new Proxy(oldNewGame5, {
     }
   }
 };
-MauzoEvolutionStage1()
 function MauzoEvolutionStage2(tick) {
   if (G.achievByName['mausoleum'].won) {
     if (G.achievByName['mausoleum'].won > 1 && G.achievByName['mausoleum'].won < 3 ) {
