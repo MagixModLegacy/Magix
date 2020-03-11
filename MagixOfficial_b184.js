3547792a0614e78bef7babfef197c84ba02590b9
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-03-11 06:29:50 +0100

    Update MagixOfficial_b184.js

--- MagixOfficial_b184.js
+++ MagixOfficial_b184.js
@@ -3974,7 +3974,7 @@ G.writeMSettingButton=function(obj)
 		category:'gear',
 		tick:function(me,tick)
 		{
-			var toSpoil=me.amount*0.00003;
+			var toSpoil=me.amount*0.00009;
 			var spent=G.lose(me.name,randomFloor(toSpoil),'decay');
 		},
 	});
