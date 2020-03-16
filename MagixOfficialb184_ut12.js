2a1e6345198f45cb7934b4be38cd7ea341a4d184
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-03-16 08:44:07 +0100

    Update MagixOfficialb184_ut12.js

--- MagixOfficialb184_ut12.js
+++ MagixOfficialb184_ut12.js
@@ -7224,8 +7224,8 @@ new G.Unit({
 	});
 		new G.Achiev({
 		tier:2,
-		wideIcon:[27,0,'magixmod'],
-		icon:[28,0,'magixmod'],
+		wideIcon:[27,20,'magixmod'],
+		icon:[28,20,'magixmod'],
 		name:'<font color="DA4f37">Mausoleum eternal</font>',
 		desc:'You have been laid to rest serveral times in the Mausoleum , an ancient stone monument the purpose of which takes root in archaic religious thought. Evolved to unforgetable historical monument. <b>Evolve [mausoleum] to stage 10/10 then ascend by it 11th time to obtain this massive fast tick bonus. <li><font color="aqua">In addition obtaining this achievement doubles chance to summon [belief in the afterlife] trait in each next run after obtaining this achievement.</font></li></b>',
 		fromWonder:'<font color="DA4f37">Mausoleum eternal</font>',
