a30c05d76db454d414bb8f4b777ca19854575ed8
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-03-17 08:27:36 +0100

    Update and rename MagixOfficialb184_ut34.js to MagixOfficialb184_ut35.js

--- MagixOfficialb184_ut35.js
+++ MagixOfficialb184_ut35.js
@@ -9632,7 +9632,7 @@ G.NewGameConfirm = new Proxy(oldNewGame5, {
 		desc:'@People can build bigger [kiln]s that are more efficient. <>Increases efficiency of all [Kiln] types by additive 150% but doubles upkeep of this unit. <>In additions this tech changes their visual look.',
 		icon:[21,21,'magixmod'],
 		cost:{'insight II':15},
-		req:{'Improved rhetoric':true,'construction II':true,'masonry':true,'weaving II':true},
+		req:{'Improved rhetoric':true,'construction II':true,'masonry':true,'weaving II':true,'prospecting II':true},
 		effects:[
 		],
 		chance:4,
