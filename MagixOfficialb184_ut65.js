cebdf6a0c6649866d2e4db27bbc803a695bda5fe
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-03-19 09:04:35 +0100

    Update and rename MagixOfficialb184_ut64.js to MagixOfficialb184_ut65.js

--- MagixOfficialb184_ut65.js
+++ MagixOfficialb184_ut65.js
@@ -168,8 +168,8 @@ G.props['fastTicksOnResearch']=150;
 			var stat=G.getRes('health').amount/pop;
 			var text='unknown';if (stat<=-200) text='dreadful'; else if (stat<=-100) text='sickly'; else if (stat<=-50) text='low'; else if (stat<50) text='average'; else if (stat<100) text='good'; else if (stat<=200) text='gleaming'; else if (stat>=200) text='examplary';
 			toParse+='Health : <b>'+text+'</b>//';
-			var stat= G.techN;
-			var text='none';if (stat>=20) text='archaic'; else if (stat>=85) text='almost-medieval'; else if (stat>=130) text='medieval'; else if (stat>=200) text='primarily decent'; else if (stat>=250) text='decent'; else if (stat>=325) text='primarily advanced'; else if (stat>=400) text='advanced'; else if (stat>=500) text='advanced and educated';
+			var stat= technologiesam;
+			var text='pre-archaic';if (stat>=20) text='archaic'; else if (stat>=85) text='almost-medieval'; else if (stat>=130) text='medieval'; else if (stat>=200) text='primarily decent'; else if (stat>=250) text='decent'; else if (stat>=325) text='primarily advanced'; else if (stat>=400) text='advanced'; else if (stat>=500) text='advanced and educated';
 			toParse+='Technological progress stage: <b>'+text+'</b>//';
 		}
 		else toParse+='All '+G.getName('inhabs')+' have died out.';
