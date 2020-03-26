d5ccb7535299a08971b07ce44a575049e2593945
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-03-26 09:02:57 +0100

    Update and rename MagixOfficialb184_ut114.js to MagixOfficialb184_ut115.js

--- MagixOfficialb184_ut115.js
+++ MagixOfficialb184_ut115.js
@@ -398,20 +398,6 @@ if (!document.getElementById(cssId))
     link.media = 'all';
     head.appendChild(link);
 }
-	if (G.checkPolicy('Theme changer')=='on'){
-		var cssId = 'greenthemeCss';  
-if (!document.getElementById(cssId))
-{
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    link.id   = cssId;
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/GreenTheme/greentheme.css';
-    link.media = 'all';
-    head.appendChild(link);
-}
-	}
 	/*=====================================================================================
 	RESOURCES
 	=======================================================================================*/
@@ -10530,6 +10516,22 @@ G.NewGameConfirm = new Proxy(oldNewGame5, {
 		category:'seasonal',
 		hidden:true,
 	});
+	/*============================================================================
+	THEMES
+	============================================================================*/
+		if (G.checkPolicy('Theme changer')=='on'){
+		var cssId = 'greenthemeCss';  
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/GreenTheme/greentheme.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
 	/*=====================================================================================
 	LANDS
 	=======================================================================================*/
