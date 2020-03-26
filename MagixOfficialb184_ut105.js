2d124ed56beca73ac52187067e840b654a0573ad
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-03-26 07:58:41 +0100

    Update and rename MagixOfficialb184_ut104.js to MagixOfficialb184_ut105.js

--- MagixOfficialb184_ut105.js
+++ MagixOfficialb184_ut105.js
@@ -4230,7 +4230,7 @@ G.writeMSettingButton=function(obj)
 			G.middleText('- Completed <font color="lime">Traitsman</font> achievement -')
 			}
 			if((G.getRes('insight II').amount) == (G.getRes('wisdom II').amount) && G.achievByName['Extremely smart'].won == 0){; //Extremely smart achievement
-			G.achievByName['Extemely smart'].won = 1
+			G.achievByName['Extremely smart'].won = 1
 			G.middleText('- Completed <font color="purple">Extremely smart</font> achievement -')
 			}
 		},
