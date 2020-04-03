36c491def35cd9a234801b9cbe1868436e9e5509
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-04-03 08:48:55 +0200

    Update and rename MagixOfficial_b184_ut181.js to MagixOfficial_b184_ut182.js

--- MagixOfficial_b184_ut182.js
+++ MagixOfficial_b184_ut182.js
@@ -7163,7 +7163,7 @@ new G.Unit({
 }},
 			{type:'function',func:function(me){
                 if(G.getRes('insight').amount>=600){
-                  return  {type:'convert',from:{'insight':500},into:{'insight II':1},every:9,mode:'insight',req:{'Essential conversion tank overclock I':true}}
+                  return  spent=G.lose('insight',500,'conversion');G.gain('insight II',1,'Conversion tank');
                 }
 }},
 						{type:'function',func:function(me){
