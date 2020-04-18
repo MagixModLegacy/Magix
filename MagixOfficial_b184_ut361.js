8227df5a40a381954b70076d24a7e87e55e98515
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-04-18 09:31:40 +0200

    Update and rename MagixOfficial_b184_ut360.js to MagixOfficial_b184_ut361.js

--- MagixOfficial_b184_ut361.js
+++ MagixOfficial_b184_ut361.js
@@ -12150,11 +12150,9 @@ G.NewGameConfirm = new Proxy(oldNewGameMagical, {
 	});
 	G.funcs['new game blurb']=function()
 	{   
-		let cult1print=false
-		if(G.achievByName['Sacrificed for culture'].won >0){
-			cult1print=true
-		}
-		var cult1print=G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('culture'))+'"></div><div class="freelabel">x3</div>','3 Culture & Inspiration')+
+	const cultres=if(G.achievByName['Sacrificed by culture'].won>0){
+		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('culture'))+'"></div><div class="freelabel">x3</div>','3 Culture & Inspiration')+
+	}
 		var str=
 		'<font color="fuschia">Magix expansion has been loaded succesfully. <b>: )</b></br></font>'+
 		'<b>Your tribe :</b><div class="thingBox">'+
@@ -12163,7 +12161,7 @@ G.NewGameConfirm = new Proxy(oldNewGameMagical, {
 		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('child'))+'"></div><div class="freelabel">x2</div>','2 Children')+
 		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('herb'))+'"></div><div class="freelabel">x250</div>','250 Herbs')+
 		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('water'))+'"></div><div class="freelabel">x250</div>','250 Water')+
-		cult1print
+		cultres
 		//G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('insight'))+'"></div><div class="freelabel">x6</div>','6 Insight')+
 		//G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('influence'))+'"></div><div class="freelabel">x6</div>','6 Influence & Authority')+
 		'</div>'+
