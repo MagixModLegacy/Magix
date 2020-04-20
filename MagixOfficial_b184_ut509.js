d1dc37483be8b974db0991f6d80f268516c66dd8
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-04-20 13:42:55 +0200

    Update and rename MagixOfficial_b184_ut508.js to MagixOfficial_b184_ut509.js

--- MagixOfficial_b184_ut509.js
+++ MagixOfficial_b184_ut509.js
@@ -11136,7 +11136,7 @@ G.NewGameConfirm = new Proxy(oldNewGameMagical, {
         G.getDict('bazaar_sell').effects.push({type:'mult',value:1.5,req:{'Backshift':true}});
         G.getDict('market_buy').effects.push({type:'mult',value:1.5,req:{'Backshift':true}});
         G.getDict('market_sell').effects.push({type:'mult',value:1.5,req:{'Backshift':true}});
-G.getDict('market_sell').modes['dark essence']={
+G.setDict('market_sell').modes['dark essence']={
   name:'Dark essence',
   icon:[1,3,'magixmod'],
   desc:'ttte',
