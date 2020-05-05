a301fd1ff29272006fbd5d59dbcc1b5ddce583b0
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-05-05 10:02:15 +0200

    Update and rename MagixOfficial_b184_ut640.js to MagixOfficial_b184_ut641.js

--- MagixOfficial_b184_ut641.js
+++ MagixOfficial_b184_ut641.js
@@ -11739,7 +11739,7 @@ G.NewGameConfirm = new Proxy(oldNewGameGodTemple, {
 		var str='';
 		str+=
 			'<div class="regularWrapper">'+
-			G.textWithTooltip('?','<div style="width:240px;text-align:left;"><div class="par">Inside the Pantheon you may take on a trial. Available trials depend on chosen seraphin. Each one has different trial. Almost each of the trials are repeatable.<br> For each trial victory you will gain a Victory point. <br>For winning a trial for the first time you will gain special bonuses. <br>Second victory on the same trial will make you get 2 extra Victory Points so in total the trial you won twice gives you 3 Victory Points. //Each next trial playing after a first victory of a trial will be harder(example: on map you will find less land than you were during beating the trial for the first time and so on) </div></div>','infoButton')+
+			str+=G.textWithTooltip('?','<div style="width:240px;text-align:left;"><div class="par">Inside the Pantheon you may take on a trial. Available trials depend on chosen seraphin. Each one has different trial. Almost each of the trials are repeatable.<br> For each trial victory you will gain a Victory point. <br>For winning a trial for the first time you will gain special bonuses. <br>Second victory on the same trial will make you get 2 extra Victory Points so in total the trial you won twice gives you 3 Victory Points. //Each next trial playing after a first victory of a trial will be harder(example: on map you will find less land than you were during beating the trial for the first time and so on) </div></div>','infoButton')+
 			'<div class="fullCenteredOuter"><div id="policyBox" class="thingBox fullCenteredInner"></div></div></div>';
 		l('policyDiv').innerHTML=str;
 	}
