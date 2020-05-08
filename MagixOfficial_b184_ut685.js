4094ee272a6a0040f87fbe423783231c68dfbc8d
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-05-08 09:59:38 +0200

    Update and rename MagixOfficial_b184_ut684.js to MagixOfficial_b184_ut685.js

--- MagixOfficial_b184_ut685.js
+++ MagixOfficial_b184_ut685.js
@@ -5031,7 +5031,7 @@ if (!document.getElementById(cssId))
 			}
 		}
 	}
-	var chranos=function(){(G.getRes('Watermelon seeds').amount/1000)};
+	var chranos=function(){1+(G.getRes('Watermelon seeds').amount/1000)};
 	new G.Unit({
 		name:'gatherer',
 		startWith:5,
@@ -5052,7 +5052,7 @@ if (!document.getElementById(cssId))
 			{type:'mult',value:1.125,req:{'Focused gathering':true,'<font color="maroon">Caretaking</font>':true}},
 			{type:'mult',value:0.8,req:{'se12':'on'}},
 			{type:'mult',value:0.85,req:{'se07':'on'}},
-			{type:'mult',value:1+chranos},
+			{type:'mult',value:chranos},
 		],
 		req:{'tribalism':true},
 		category:'production',
