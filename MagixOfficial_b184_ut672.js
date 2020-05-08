62d985e5cb3a5cc2855aa77f2b7b0c11e0129dee
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-05-08 08:36:39 +0200

    Update and rename MagixOfficial_b184_ut671.js to MagixOfficial_b184_ut672.js

--- MagixOfficial_b184_ut672.js
+++ MagixOfficial_b184_ut672.js
@@ -5044,6 +5044,7 @@ if (!document.getElementById(cssId))
 			{type:'mult',value:1.125,req:{'Focused gathering':true,'<font color="maroon">Caretaking</font>':true}},
 			{type:'mult',value:0.8,req:{'se12':'on'}},
 			{type:'mult',value:0.85,req:{'se07':'on'}},
+			{type:'mult',value:1+15},
 		],
 		req:{'tribalism':true},
 		category:'production',
