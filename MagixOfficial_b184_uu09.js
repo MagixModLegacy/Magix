ac3e8673e50be9346a0dcb6a54b5b5f37ccd748e
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-05-19 13:16:22 +0200

    Update and rename MagixOfficial_b184_uu08.js to MagixOfficial_b184_uu09.js

--- MagixOfficial_b184_uu09.js
+++ MagixOfficial_b184_uu09.js
@@ -11940,8 +11940,16 @@ let tabload = false
         }
 	G.tabs=
 	[
-			{name:'Production',id:'unit',update:'unit',desc:'Recruit units and create buildings.'}
-		]
+		{name:'Production',id:'unit',update:'unit',desc:'Recruit units and create buildings.'},
+		{name:'Territory',id:'land',update:'land',showMap:true,desc:'View the world map, inspect explored territory and see your natural resources.'},
+		{name:'Diplomacy',id:'diplo',showMap:true,desc:'View and interact with other civilizations; conduct trade and send armies.'},//later
+		{name:'Policies',id:'policy',update:'policy',desc:'Use your influence to enact policies that change the way your civilization functions.'},
+		{name:'Traits',id:'trait',update:'trait',desc:'View traits and edit your civilization\'s properties.'},
+		{name:'Research',id:'tech',update:'tech',desc:'Purchase new technologies that improve your civilization and unlock new units.'},
+		{name:'Settings',id:'settings',popup:true,addClass:'right',desc:'Change the game\'s settings.'},
+		{name:'Update log',id:'updates',popup:true,addClass:'right',desc:'View the game\'s version history and other information.'},
+		{name:'Legacy',id:'legacy',popup:true,addClass:'right',desc:'View your legacy stats and achievements.'}
+	]
 	/*=====================================================================================
 	POLICIES
 	=======================================================================================*/
