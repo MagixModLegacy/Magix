b7fc4ab42e2d12f74f7e14cfaedab179fa2e8a00
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-05-22 14:09:26 +0200

    Update and rename MagixOfficial_b184_91b.js to MagixOfficial_b184_92b.js

--- MagixOfficial_b184_92b.js
+++ MagixOfficial_b184_92b.js
@@ -9,7 +9,7 @@ func:function(){
 //READ THIS: All rights reserved to mod creator and people that were helping the main creator with coding. Mod creator rejects law to copying icons from icon sheets used for this mod. All noticed plagiariasm will be punished. Copyright: 2020
 //===========================
 G.props['fastTicksOnResearch']=150;
-	let t1start = false
+	let t1start = 0
 		G.funcs['new game blurb']=function()
 	{   
 		var str=
@@ -50,18 +50,18 @@ G.props['fastTicksOnResearch']=150;
 	{
 		if (G.on)
 		{
-			if(t1start==true)
+			if(t1start==1)
 			{
 			var strb='';
-			strb+='It is now the year <b>'+(G.year+1)+'</b>.<br>';
-			strb+='Report for last year :<br>';
-			strb+='&bull; <b>Births</b> : '+B(G.getRes('born this year').amount)+'<br>';
-			strb+='&bull; <b>Deaths</b> : '+B(G.getRes('died this year').amount)+'<br>';
-			strb+='&bull; <b>Insight descended</b> : '+B(G.getRes('died this year').amount)+'<br>';
+			str+='It is now the year <b>'+(G.year+1)+'</b>.<br>';
+			str+='Report for last year :<br>';
+			str+='&bull; <b>Births</b> : '+B(G.getRes('born this year').amount)+'<br>';
+			str+='&bull; <b>Deaths</b> : '+B(G.getRes('died this year').amount)+'<br>';
+			str+='&bull; <b>Insight descended</b> : '+B(G.getRes('died this year').amount)+'<br>';
 			G.getRes('born this year').amount=0;
 			G.getRes('died this year').amount=0;
 			G.Message({type:'important',text:strb,icon:[0,3]});
-			}else if(t1start==false){
+			}else if(t1start==0){
 				var str='';
 			str+='It is now the year <b>'+(G.year+1)+'</b>.<br>';
 			str+='Report for last year :<br>';
