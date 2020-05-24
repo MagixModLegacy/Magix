761d420798c0eb180c78ee1a7166509cca64330b
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-05-24 08:38:51 +0200

    Update and rename MagixOfficial_b184_154b.js to MagixOfficial_b184_155b.js

--- MagixOfficial_b184_155b.js
+++ MagixOfficial_b184_155b.js
@@ -8993,9 +8993,12 @@ getCosts:function()
 		req:{'speech':true},
 		effects:[
 			{type:'provide res',what:{'inspiration':30,'wisdom':30}},
-			{type:'function',func:function(){G.Message({
+			{type:'function',func:function(){if(G.has('language')){
+				G.Message({
 				type:'important',text:'Now while talking to your people they understand you more. And they understand themselves each other',
 				icon:[1,28,'magixmod']})
+			}
+			}};
 		],
 		chance:3,
 	});
