c1ee91f5176e927b9930375b3e89c591eeec7b84
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-06-17 12:06:45 +0200

    Update and rename MagixOfficial_b184_278b.js to MagixOfficial_b184_279b.js

--- MagixOfficial_b184_279b.js
+++ MagixOfficial_b184_279b.js
@@ -8,10 +8,11 @@ sheets:{'magixmod':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/magixmod.p
 func:function(){
 //READ THIS: All rights reserved to mod creator and people that were helping the main creator with coding. Mod creator rejects law to copying icons from icon sheets used for this mod. All noticed plagiariasm will be punished. Copyright: 2020
 //===========================
-	new G.trait({
+new G.Trait({
 name:'ast1',
 displayName:'. . .',
 desc:'You seem powerful. Probably [Guru] can now make more [science].',
+chance:10
 });
 
 G.props['fastTicksOnResearch']=150;
