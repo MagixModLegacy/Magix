6cbaf13f12cdcf3b221f0d57925c04dcc6891d1b
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-06-23 15:46:11 +0200

    Update and rename MagixOfficial_b184_326b.js to MagixOfficial_b184_327b.js

--- MagixOfficial_b184_327b.js
+++ MagixOfficial_b184_327b.js
@@ -1225,7 +1225,7 @@ G.writeMSettingButton=function(obj)
 		str+='<div class="par">Ascensions : <b>'+G.selfUpdatingText(function(){return B(G.resets);})+'</b></div>';
 		str+='<div class="par">Victory points: <b>'+G.selfUpdatingText(function(){return B(G.getRes('victory point').amount);})+'</b></div>';
 		str+='<div class="par">Successful trial accomplishments: <b>'+G.selfUpdatingText(function(){return B(G.achievByName['Patience'].won+G.achievByName['Unhappy'].won+G.achievByName['Cultural'].won+G.achievByName['Hunted'].won+G.achievByName['Unfishy'].won+G.achievByName['Ocean'].won+G.achievByName['Herbalism'].won+G.achievByName['Buried'].won+G.achievByName['Underground'].won+G.achievByName['Pocket'].won+G.achievByName['Faithful'].won+G.achievByName['Dreamy'].won);})+'</b></div>';
-		str+='<div class="par">Most technologies obtained : <b>'+G.selfUpdatingText(function(){return B(Math.max(G.techN)));})+'</b></div>';
+		str+='<div class="par">Most researches completed(in one run): <b>'+G.selfUpdatingText(function(){return G.BT(Math.max(G.techN));})+'</b></div>';
 		str+='</div>';
 		str+='<div class="scrollBox underTitle" style="width:380px;right:0px;left:auto;background:rgba(0,0,0,0.25);">';
 		if (G.sequence=='main')
