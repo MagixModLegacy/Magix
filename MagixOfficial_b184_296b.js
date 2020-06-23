21b29814745996ee3063b44a8db3c52c56d93eed
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-06-23 09:52:20 +0200

    Update and rename MagixOfficial_b184_295b.js to MagixOfficial_b184_296b.js

--- MagixOfficial_b184_296b.js
+++ MagixOfficial_b184_296b.js
@@ -32,9 +32,9 @@ G.props['fastTicksOnResearch']=150;
 		G.funcs['new game blurb']=function()
 	{   
 			if(G.achievByName['Democration'].won>=1){
-				var democ=write("G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('influence'))+'"></div><div class="freelabel">x6</div>','6 Influence & Authority')+")
+				var democ=document.write("G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('influence'))+'"></div><div class="freelabel">x6</div>','6 Influence & Authority')+")
 						 }else{
-						 var democ=write("")
+						 var democ=document.write("")
 				}
 		var str=
 		'<font color="fuschia">Magix expansion has been loaded succesfully. <b>: )</b></br></font>'+
