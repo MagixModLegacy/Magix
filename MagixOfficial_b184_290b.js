541cf69fa4c4d17d6a065a2aec3d2150f1a45b3a
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-06-23 09:36:07 +0200

    Update and rename MagixOfficial_b184_289b.js to MagixOfficial_b184_290b.js

--- MagixOfficial_b184_290b.js
+++ MagixOfficial_b184_290b.js
@@ -31,6 +31,7 @@ G.props['fastTicksOnResearch']=150;
 	let st11=false
 		G.funcs['new game blurb']=function()
 	{   
+			if(G.achievByName['Democration'].won==0){
 		var str=
 		'<font color="fuschia">Magix expansion has been loaded succesfully. <b>: )</b></br></font>'+
 		'<b>Your tribe :</b><div class="thingBox">'+
@@ -45,6 +46,22 @@ G.props['fastTicksOnResearch']=150;
 		'<div class="par fancyText bitBiggerText">Your tribe finds a place to settle in the wilderness.<br>Resources are scarce, and everyone starts foraging.</div>'+
 		'<div class="par fancyText bitBiggerText">You emerge as the tribe\'s leader. <br>These people... They call you :</div>';
 		return str;
+			}else{
+			var str=
+		'<font color="fuschia">Magix expansion has been loaded succesfully. <b>: )</b></br></font>'+
+		'<b>Your tribe :</b><div class="thingBox">'+
+		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('adult'))+'"></div><div class="freelabel">x5</div>','5 Adults')+
+		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('elder'))+'"></div><div class="freelabel">x1</div>','1 Elder')+
+		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('child'))+'"></div><div class="freelabel">x2</div>','2 Children')+
+		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('herb'))+'"></div><div class="freelabel">x250</div>','250 Herbs')+
+		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('water'))+'"></div><div class="freelabel">x250</div>','250 Water')+
+		//G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('insight'))+'"></div><div class="freelabel">x6</div>','6 Insight')+
+		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('influence'))+'"></div><div class="freelabel">x6</div>','6 Influence & Authority')+
+		'</div>'+
+		'<div class="par fancyText bitBiggerText">Your tribe finds a place to settle in the wilderness.<br>Resources are scarce, and everyone starts foraging.</div>'+
+		'<div class="par fancyText bitBiggerText">You emerge as the tribe\'s leader. <br>These people... They call you :</div>';
+		return str;
+			}
 	}
 	//////////////////////////////////////
 	G.funcs['new game']=function()
