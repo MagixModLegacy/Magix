66fe4ebf35edfbe1b952ac0745bc3eaab98c1807
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-06-24 22:14:33 +0200

    Update and rename MagixOfficial_b184_367b.js to MagixOfficial_b184_368b.js

--- MagixOfficial_b184_368b.js
+++ MagixOfficial_b184_368b.js
@@ -1300,10 +1300,10 @@ G.writeMSettingButton=function(obj)
 		var str=''+
 		'<div class="flourishL"></div><div class="framed fancyText bgMid" style="display:inline-block;padding:8px 12px;font-weight:bold;font-size:18px;font-variant:small-caps;" id="date">-</div><div class="flourishR"></div><br>'+
 		'<div class="flourish2L"></div>'+
-		'<div id="fastTicks" class="framed" style="display:inline-block;padding-left:8px;padding-right:8px;font-weight:bold;"><font color="#ddff00">0</font> ('+G.fastTicks+' fast ticks)</div>'+
+		'<div id="fastTicks" class="framed" style="display:inline-block;padding-left:8px;padding-right:8px;font-weight:bold;linear-gradient(to right, red, orange, yellow, lime, blue, purple, fuchsia);">0</div>'+
 		G.button({id:'pauseButton',
 			text:'<div class="image" style="width:9px;background:url(img/playButtons.png) 0px 0px;"></div>',
-			tooltip:'Time will be stopped.<br>Generates fast ticks.',
+			tooltip:'Time will be stopped.<br>Generates fast ticks(1 fast tick per second of paused).',
 			onclick:function(){G.setSetting('paused',1);}
 		})+
 		G.button({id:'playButton',
@@ -1317,6 +1317,13 @@ G.writeMSettingButton=function(obj)
 			onclick:function(){if (G.fastTicks>0) {G.setSetting('paused',0);G.setSetting('fast',1);}}
 		})+
 		'<div class="flourish2R"></div>';
+	}
+		G.FileSave=function()
+	{
+		var filename='MagixLegacySave';
+		var text=G.Export();
+		var blob=new Blob([text],{type:'text/plain;charset=utf-8'});
+		saveAs(blob,filename+'.txt');
 	}
 	/*=====================================================================================
 	RESOURCES
