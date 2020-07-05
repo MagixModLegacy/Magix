814bde9cf735cbb239deb872770fc8aad7e666d9
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-05 16:15:51 +0200

    Update and rename MagixOfficial_b184_500b.js to MagixOfficial_b184_501b.js

--- MagixOfficial_b184_501b.js
+++ MagixOfficial_b184_501b.js
@@ -5991,8 +5991,8 @@ if (!document.getElementById(cssId))
 		name:'blood',
 		desc:'You gain blood each year from Madness victims equal to murdered people. Required to glory Bersaria and to research next things with [fear of death] active. You start with 200 [blood] in that case.',
 		icon:[33,6,'magixmod'],
-		startWith:350
-		category:''
+		startWith:350,
+		category:'',
 	});
 		/*=====================================================================================
 	ACHIEVEMENTS
