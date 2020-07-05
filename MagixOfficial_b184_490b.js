bd4d4638a026d1ec636ec14ea8cd937a15f57684
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-05 15:49:35 +0200

    Update and rename MagixOfficial_b184_489b.js to MagixOfficial_b184_490b.js

--- MagixOfficial_b184_490b.js
+++ MagixOfficial_b184_490b.js
@@ -9857,7 +9857,7 @@ getCosts:function()
             if (G.hasNot('Eotm')){
               return { 'insight' : calcCost('wisdom') }
             }else if (G.hasNot('Eotm') && G.has('t2') && G.has('fear of death')){
-              return { 'insight' : calcCost('wisdom') , 'blood':(1+(G.year/5)*(G.achievByName['Unhappy'].won+1))}
+              return { 'insight' : calcCost('wisdom') , 'blood': calcCost('wisdom', 0.01+(G.year/10)}
             }else if(G.has('Eotm') && G.hasNot('do we need that much science?')){
             return { 'insight II' : calcCost('wisdom II'), 'science': calcCost('education', 0.2) }
 	    }else if(G.has('Eotm') && G.has('do we need that much science?')){
@@ -9937,7 +9937,7 @@ getCosts:function()
 			}else if(G.hasNot('Eotm') && G.has('t2') && G.has('fear of death')){
 			return '<div class="info"><div class="par">'+(this.choices.length==0?'Generate new research opportunities.<br>The cost scales with your <b>Wisdom</b> resource.<br>The blood cost scales with amount of techs owned.(you currently own: '+G.getRes('blood').amount+'Blood)':'Reroll into new research opportunities if none of the available choices suit you.<br>Cost increases with each reroll, but will decrease again over time.')+'</div><div>Cost : '+G.getCostString(this.getCosts(),true)+'.</div></div>';
 			}
-			if (G.has('Eotm')){
+			else if (G.has('Eotm')){
 			return '<div class="info"><div class="par">'+(this.choices.length==0?'Generate new research opportunities.<br>The cost scales with your <b>Wisdom II & Education</b> resources.':'Reroll into new research opportunities if none of the available choices suit you.<br>Cost increases with each reroll, but will decrease again over time.')+'</div><div>Cost : '+G.getCostString(this.getCosts(),true)+'.</div></div>';
 			}
 		}
