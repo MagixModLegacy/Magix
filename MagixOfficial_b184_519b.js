3d4f0c1ae4cbab8c7663756ded0ac0dbb15bf52f
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-12 08:48:55 +0200

    Update and rename MagixOfficial_b184_518b.js to MagixOfficial_b184_519b.js

--- MagixOfficial_b184_519b.js
+++ MagixOfficial_b184_519b.js
@@ -5870,7 +5870,7 @@ if (!document.getElementById(cssId))
 				G.getDict('pagoda of passing time').cost={'basic building materials':225}
 			}
 			//STORAGE NERFS
-			G.getDict('Fire essence storage').cost={'basic building materials':(100*(G.getUnitAmount('Fire essence storage')+1/10)),'glass':(200*(G.getUnitAmount('Fire essence storage')+1/8))};
+			G.getDict('Fire essence storage').cost={'basic building materials':(50*(G.getUnitAmount('Fire essence storage')+1/15)),'glass':(100*(G.getUnitAmount('Fire essence storage')+1/15))};
 		},
 		getDisplayAmount:researchGetDisplayAmount,
 		whenGathered:researchWhenGathered,
