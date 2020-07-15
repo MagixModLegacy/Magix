b6290468e8518f37b2383ab8e818f5cbdb36d8f2
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-15 12:04:01 +0200

    Update and rename MagixOfficial_b184_544b.js to MagixOfficial_b184_545b.js

--- MagixOfficial_b184_545b.js
+++ MagixOfficial_b184_545b.js
@@ -378,6 +378,10 @@ G.props['fastTicksOnResearch']=150;
 	{
 		return 'url(https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png),url(https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png)';
 	}
+		G.getLandIconBGpos=function(land)
+	{
+		return (-32*land.image-2)+'px '+(-2*32-2)+'px,'+(-32*land.image-2)+'px '+(-0*32-2)+'px';
+	}
 	G.LoadResources=function()
 	{
 		var resources=[
@@ -699,6 +703,10 @@ G.props['fastTicksOnResearch']=150;
 		G.getLandIconBG=function(land)
 	{
 		return 'url(https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png),url(https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png)';
+	}
+		G.getLandIconBGpos=function(land)
+	{
+		return (-32*land.image-2)+'px '+(-2*32-2)+'px,'+(-32*land.image-2)+'px '+(-0*32-2)+'px';
 	}
 	G.LoadResources=function()
 	{
