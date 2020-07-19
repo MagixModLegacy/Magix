9727bad85fa8db3ce09f31789dc2a5c6a7a5da09
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-19 12:53:21 +0200

    Update and rename MagixOfficial_b184_563b.js to MagixOfficial_b184_564b.js

--- MagixOfficial_b184_564b.js
+++ MagixOfficial_b184_564b.js
@@ -919,9 +919,9 @@ G.props['fastTicksOnResearch']=150;
 				}
 		}
 		if(G.checkPolicy('sleepy insight').mode=="+3"){
+			var bonus7=(Math.floor() * 1.35)+15;
 			var insighter=function(){
 				if(G.getRes('chance').amount<=70.25){
-					var bonus7=(Math.floor() * 1.35)+15;
 					G.gain('insight',bonus7,'Sleepy Insight');
 				}
 			}
