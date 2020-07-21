206090d19e36b1eaa8b7bb9b0cc61749df33a6b9
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-21 14:03:12 +0200

    Update and rename MagixOfficial_b184_586b.js to MagixOfficial_b184_587b.js

--- MagixOfficial_b184_587b.js
+++ MagixOfficial_b184_587b.js
@@ -10093,7 +10093,7 @@ getCosts:function()
 		buttonTooltip:function()
 		{
 			if(G.hasNot('Eotm') && G.has('t2')){
-			return '<div class="info"><div class="par">'+(this.choices.length==0?'Generate new research opportunities.<br>The cost scales with your <b>Wisdom</b> resource.<br>The blood cost scales with amount of techs owned.':'Reroll into new research opportunities if none of the available choices suit you.<br>Cost increases with each reroll, but will decrease again over time.')+'</div><div>Cost : '+G.getCostString(this.getCosts(),true)+'.</div></div>';
+			return '<div class="info"><div class="par">'+(this.choices.length==0?'Generate new research opportunities.<br>The cost scales with your <b>Wisdom</b> resource.<br>The blood cost scales with <b>Wisdom</b> resource as well.':'Reroll into new research opportunities if none of the available choices suit you.<br>Cost increases with each reroll, but will decrease again over time.')+'</div><div>Cost : '+G.getCostString(this.getCosts(),true)+'.</div></div>';
 			}
 			else if (G.hasNot('Eotm')){
 			return '<div class="info"><div class="par">'+(this.choices.length==0?'Generate new research opportunities.<br>The cost scales with your <b>Wisdom</b> resource.':'Reroll into new research opportunities if none of the available choices suit you.<br>Cost increases with each reroll, but will decrease again over time.')+'</div><div>Cost : '+G.getCostString(this.getCosts(),true)+'.</div></div>';
