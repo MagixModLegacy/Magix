03660208c31009a8683fb6727f1f0130fcf12105
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-25 11:45:34 +0200

    Update and rename MagixOfficial_b184_647b.js to MagixOfficial_b184_648b.js

--- MagixOfficial_b184_648b.js
+++ MagixOfficial_b184_648b.js
@@ -8,7 +8,7 @@ sheets:{'magixmod':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/magixmod.p
 func:function(){
 //READ THIS: All rights reserved to mod creator and people that were helping the main creator with coding. Mod creator rejects law to copying icons from icon sheets used for this mod. All noticed plagiariasm will be punished. Copyright: 2020
 //===========================
-	var img=Pic('https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png');
+	img=Pic('https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png');
 var cssId = 'betaCss'; 
 if (!document.getElementById(cssId))
 {
