d4050ca1748529da4fd0cbd306a79812ab6e10fa
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-07-26 13:21:10 +0200

    Update and rename MagixOfficial_b184_667b.js to MagixOfficial_b184_668b.js

--- MagixOfficial_b184_668b.js
+++ MagixOfficial_b184_668b.js
@@ -1121,9 +1121,9 @@ G.props['fastTicksOnResearch']=150;
 	
 		G.Message({type:'good',mergeId:'foundTile',textFunc:function(args){
 			if(args.tile.land.displayName=="Dead forest")G.achievByName['lands of despair'].won+1;if(G.achievByName['lands of despair'].won<1){G.middleText('- Completed <font color="gray">Lands of despair</font> achievement -','slow')};
-			if (args.count==1) return 'Our explorers have found a new tile : <b>'+args.tile.land.displayName;+'</b>.';
-			else return 'Our explorers have found '+B(args.count)+' new tiles; the latest is <b>'+args.tile.land.displayName;+'</b>.';
-			
+			if (args.count==1){ return 'Our explorers have found a new tile : <b>'+args.tile.land.displayName;+'</b>.';
+					  }else return{ 'Our explorers have found '+B(args.count)+' new tiles; the latest is <b>'+args.tile.land.displayName;+'</b>.';
+						      }
 		},args:{tile:tile,count:1},icon:[14,4]});
 
 	}
