fd70544b2295f56945ce10453d9c1557af166ee4
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-08-05 16:50:13 +0200

    Update and rename MagixOfficial_b184_710b.js to MagixOfficial_b184_711b.js

--- MagixOfficial_b184_711b.js
+++ MagixOfficial_b184_711b.js
@@ -152,12 +152,12 @@ G.setPolicyMode=function(me,mode)
 							}
 							if (achiev)
 							{
-								if (!achiev.won) middleText='- Completed the '+achiev.displayName+' victory - <br><hr width="40"><br>'+if(randomTxtId>=0 && randomTxtId<=1) return '<small>Good job : )</small>';
+								if (!achiev.won) middleText='- Completed the '+achiev.displayName+' victory - <br><hr width="40"><br>'+function(){if(randomTxtId>=0 && randomTxtId<=1) return '<small>Good job : )</small>';
 							else if(randomTxtId>1 && randomTxtId<=2) return '<small>Well done : )</small>';
 							else if(randomTxtId>2 && randomTxtId<=3) return '<small>Neat-O</small>';
 							else if(randomTxtId>3 && randomTxtId<=4) return '<small>Oh yeah!</small>';
 							else if(randomTxtId>4 && randomTxtId<=5) return '<small>Cool</small>';
-							else if(randomTxtId>5 && randomTxtId<=6) return '<small>Snazzy</small>';+'';
+							else if(randomTxtId>5 && randomTxtId<=6) return '<small>Snazzy</small>';}+'';
 								achiev.won++;
 							}
 							G.resets++;
