4254f9e9e28105081363289cdc59b075022110d6
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-08-05 17:14:10 +0200

    Update MagixOfficial_b184_717b.js

--- MagixOfficial_b184_717b.js
+++ MagixOfficial_b184_717b.js
@@ -8,10 +8,6 @@ sheets:{'magixmod':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/magixmod.p
 func:function(){
 //READ THIS: All rights reserved to mod creator and people that were helping the main creator with coding. Mod creator rejects law to copying icons from icon sheets used for this mod. All noticed plagiariasm will be punished. Copyright: 2020
 //===========================
-	
-	var randomTxtId=Math.floor(Math.random()*6);
-	var WonderAchievTxt=''
-	var txtAchiev=function(){if(randomTxtId>=0 && randomTxtId<=1) return WonderAchievTxt='<small>Good job : )</small>';else if(randomTxtId>1 && randomTxtId<=2) return WonderAchievTxt='<small>Well done : )</small>';else if(randomTxtId>2 && randomTxtId<=3) return WonderAchievTxt='<small>Neat-O</small>';else if(randomTxtId>3 && randomTxtId<=4) return WonderAchievTxt='<small>Oh yeah!</small>';else if(randomTxtId>4 && randomTxtId<=5) return WonderAchievTxt='<small>Cool</small>';else if(randomTxtId>5 && randomTxtId<=6) return WonderAchievTxt='<small>Snazzy</small>'}
 	img=Pic('https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png');
 var cssId = 'betaCss'; 
 if (!document.getElementById(cssId))
@@ -155,7 +151,7 @@ G.setPolicyMode=function(me,mode)
 							var randomTxtId=Math.floor(Math.random() * 6);	
 							if (achiev)
 							{
-								if (!achiev.won) middleText='- Completed the '+achiev.displayName+' victory - <br><hr width="40"><br>'+WonderAchievTxt+''
+								if (!achiev.won) middleText='<font color="pink">- Completed the '+achiev.displayName+' victory -</font>'
 								achiev.won++;
 							}
 							G.resets++;
