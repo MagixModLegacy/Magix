4b9678e0e403c6a16315b49ca4c5d46d839213cd
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-08-09 11:55:48 +0200

    Update and rename MagixOfficial_b184_756b.js to MagixOfficial_b184_757b.js

--- MagixOfficial_b184_757b.js
+++ MagixOfficial_b184_757b.js
@@ -1488,6 +1488,17 @@ G.props['fastTicksOnResearch']=150;
 				G.gain('influence',culture/2);
 				}
 			G.gain('cultural balance',Math.random()/2)
+			var relicChance=Math.round(Math.random()*100);
+				if(relicChance<=10 && G.has('digging')){
+					var cultChance=Math.round(Math.random()*100);
+					if(cultChance<=10)
+					{
+					G.Message({type:'bad',text:'Your people found a relic while digging underground. Sadly this relic isn\'t related to culture in any way.',icon:[3,12,8,29,'magixmod']})
+					}else{
+					G.Message({type:'good',text:'Your people found a relic while digging underground. This relic is related to culture increasing up your <b>Cultural balance</b>. Fantastic',icon:[4,12,8,29,'magixmod']})
+						G.gain('cultural balance',Math.round(Math.random()*2)+1)
+					}
+				}
 	}
 	}
 	G.props['new day lines']=[
@@ -10341,7 +10352,6 @@ if (!document.getElementById(cssId))
     		name:'<span style="color: #E0CE00">Portal to the Paradise</span>',
     		desc:'@opens a portal to a huge <b>God\'s Paradise</b>A very hard project, allowed by God.//A Dream to see Paradise, angels and much, much more comes real. You will grant +26500 paradise land at your own but you <b>must</b> follow some of God\'s rules.',
     		wideIcon:[7,4,'magixmod'],
-		wonder:'..',
     		cost:{'precious building materials':35000,'insight':1500,'faith':250,'Fire essence':45000,'Water essence':47500,'Dark essence':37500,'Wind essence':27500,'Lightning essence':37750,'Nature essence':100750,'precious metal ingot':1e4,'heavenlyTemplePoint':400},
     		effects:[
     			{type:'provide',what:{'Land of the Paradise':26500}},
