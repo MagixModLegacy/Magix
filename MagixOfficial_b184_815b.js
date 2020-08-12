2b83d5614b243d39c529305c35ee1927cc340abe
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-08-12 10:08:28 +0200

    Update and rename MagixOfficial_b184_814b.js to MagixOfficial_b184_815b.js

--- MagixOfficial_b184_815b.js
+++ MagixOfficial_b184_815b.js
@@ -1,14 +1,4 @@
-G.AddData({
-name:'Default dataset',
-author:'pelletsstarPL',
-desc:'Fit more people, discover essences which have its secret use. At the moment you can reach new dimensions which will increase your max land soon. More housing so you can fit more people. Mod utilizes vanilla part of the game by adding new modes or new units. Credits to Orteil for default dataset.',
-engineVersion:1,
-manifest:'ModManifest.js',
-sheets:{'magixmod':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/magixmod.png','seasonal':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/seasonalMagix.png','terrain':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
-func:function(){
-//READ THIS: All rights reserved to mod creator and people that were helping the main creator with coding. Mod creator rejects law to copying icons from icon sheets used for this mod. All noticed plagiariasm will be punished. Copyright: 2020
-//===========================
-	G.Launch=function()
+G.Launch=function()
 {
 
 	/*=====================================================================================
@@ -232,6 +222,17 @@ func:function(){
 		
 		G.Loop();
 	}
+G.AddData({
+name:'Default dataset',
+author:'pelletsstarPL',
+desc:'Fit more people, discover essences which have its secret use. At the moment you can reach new dimensions which will increase your max land soon. More housing so you can fit more people. Mod utilizes vanilla part of the game by adding new modes or new units. Credits to Orteil for default dataset.',
+engineVersion:1,
+manifest:'ModManifest.js',
+sheets:{'magixmod':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/magixmod.png','seasonal':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/seasonalMagix.png','terrain':'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+func:function(){
+//READ THIS: All rights reserved to mod creator and people that were helping the main creator with coding. Mod creator rejects law to copying icons from icon sheets used for this mod. All noticed plagiariasm will be punished. Copyright: 2020
+//===========================
+	
 		
 var cssId = 'betaCss'; 
 if (!document.getElementById(cssId))
