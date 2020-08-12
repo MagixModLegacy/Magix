949ec952746a455386ad4ed586ad132682c53596
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-08-12 11:48:20 +0200

    Update and rename MagixOfficial_b184_833b.js to MagixOfficial_b184_834b.js

--- MagixOfficial_b184_834b.js
+++ MagixOfficial_b184_834b.js
@@ -1,7 +1,7 @@
 //SORRY GUYS!!!
 //TO MAKE WORLD MAP SUPPORT BIOMES ADDED BY MAGIX I HAD TO PASTE THESE OVER 1K LINES OF CODE. IF THERE WILL BE "SHORTER" WAY TO FIX IT I WILL IMPLEMENT IT!
 //THE PROBLEM WAS THAT WORLD MAP OF COURSE WAS GENERATING BIOMES LIKE LAVENDER FIELDS, DEAD FOREST, SWAMPLANDS, BADLANDS BUT THEIR DISPLAY WAS LIKE: BLACK TILE
-var img = new Image();   // Create new img element
+var img = Pic();   // Create new img element
 img.src = 'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/terrainMagix.png';
 G.Map=function(type,w,h,seed)
 	{
