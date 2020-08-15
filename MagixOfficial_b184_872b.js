8570d4eff2544518ff1bbb6974fdfa68a2353ad5
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-08-15 13:03:03 +0200

    Update and rename MagixOfficial_b184_871b.js to MagixOfficial_b184_872b.js

--- MagixOfficial_b184_872b.js
+++ MagixOfficial_b184_872b.js
@@ -1861,7 +1861,6 @@ G.props['fastTicksOnResearch']=150;
 	let st10=false
 	let st11=false
 	let st12=false
-	let updated=false
 		G.funcs['new game blurb']=function()
 	{   
 		var str=
@@ -2533,11 +2532,7 @@ G.props['fastTicksOnResearch']=150;
 			G.getRes('born this year').amount=0;
 			G.getRes('died this year').amount=0;
 			G.Message({type:'important',text:str,icon:[0,3]});
-			if(!updated){
-			G.getDict('wooden coin').desc='1st tier of currency used by Pocket trial. To get 1 [silver coin] you will need: '+G.selfUpdatingText(function(){return B(100*(G.achievByName['Pocket'].won*3+1))})+' [wooden coin]s. Can be used to buy primary, archaic resources.';
-			G.getDict('silver coin').desc='1st tier of currency used by Pocket trial. To get 1 [golden coin] you will need: '+G.selfUpdatingText(function(){return B(100*(G.achievByName['Pocket'].won*3+1))})+' [wooden coin]s. Can be used to buy primary, archaic resources.';
-				updated=true;
-			}
+			
 			if(t1start==true)
 			{
 				var insight=Math.floor(Math.random() * (33/(G.achievByName['Patience'].won+1)));
@@ -7874,7 +7869,7 @@ if (!document.getElementById(cssId))
 	});
 	new G.Res({
 		name:'wooden coin',
-		desc:'1st tier of currency used by Pocket trial. To get 1 [silver coin] you will need: [wooden coin]s. Can be used to buy primary, archaic resources.',
+		desc:'1st tier of currency used by Pocket trial. To get 1 [silver coin] you will need: 100*((times you have completed Pocket*3)+1) [wooden coin]s. Can be used to buy primary, archaic resources.',
 		category:'misc',
 		icon:[5,25,'magixmod'],
 		tick:function(me,tick)
@@ -7884,7 +7879,7 @@ if (!document.getElementById(cssId))
 	});
 	new G.Res({
 		name:'silver coin',
-		desc:'2nd tier of currency used by Pocket trial. To get 1 [golden coin] you will need: [silver coin]s. Can be used to buy basic resources.',
+		desc:'2nd tier of currency used by Pocket trial. To get 1 [golden coin] you will need: 100*((times you have completed Pocket*3)+1) [silver coin]s. Can be used to buy basic resources.',
 		category:'misc',
 		icon:[6,25,'magixmod'],
 		tick:function(me,tick)
