fa617a1b697d1ac221bfd65bcbea30d758a9f1fb
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-08-21 15:34:35 +0200

    Update and rename MagixOfficial_b184_953b.js to MagixOfficial_b184_954b.js

--- MagixOfficial_b184_954b.js
+++ MagixOfficial_b184_954b.js
@@ -12023,13 +12023,11 @@ new G.Unit({
     		cost:{'archaic building materials':200,'wooden coin':90},
     		effects:[
 			{type:'function',func:function(me){
-			var toConvert=(G.achievByName['Pocket'].won*3+1)*100;
-				if(G.getRes('wooden coin').amount >= toConvert){G.lose('wooden coin',toConvert);G.gain('silver coin',1);};
+				if(G.getRes('wooden coin').amount >= (G.achievByName['Pocket'].won*3+1)*100){G.lose('wooden coin',(G.achievByName['Pocket'].won*3+1)*100);G.gain('silver coin',1);};
 			
 			},mode:'wts'},
 			{type:'function',func:function(me){
-			var toConvert=100*(G.achievByName['Pocket'].won*3+1)*100;
-				if(G.getRes('silver coin').amount >= toConvert){G.lose('silver',toConvert);G.gain('golden coin',1);};
+				if(G.getRes('silver coin').amount >= (G.achievByName['Pocket'].won*3+1)*100){G.lose('silver',(G.achievByName['Pocket'].won*3+1)*100);G.gain('golden coin',1);};
 			
 			},mode:'stg'},
     		],
