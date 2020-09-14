52f628f3848c559856463f4506cea8542e3744e3
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-09-14 18:37:27 +0200

    Update and rename MagixOfficial_b184_69c.js to MagixOfficial_b184_70c.js

--- MagixOfficial_b184_70c.js
+++ MagixOfficial_b184_70c.js
@@ -2987,7 +2987,7 @@ G.props['fastTicksOnResearch']=150;
 			if(G.getRes('wisdom').amount<100){
 		G.gain('wisdom',1)	
 		}}//year1&2 nerf
-		if(G.year==1){
+		if(G.year<=1){
 		G.gain('happiness',0.15)
 		}
 		//Time measuring tech. It will have 2 levels. Here goes the code:
@@ -2996,11 +2996,11 @@ G.props['fastTicksOnResearch']=150;
    			G.addTooltip(l('date'),function(){return '<div class="barred">Date</div><div class="par">Obtain <b>Time Measuring 1/2</b> research to display current date<br>(you\'ll see Year).<br> Despite of that you do not see current date events related to time may still occur.</div>';},{offY:-8});
 			    
 			    }
-		if(G.has('time measuring 1/2') && G.hasNot('time measuring 2/2')){
+		else if(G.has('time measuring 1/2') && G.hasNot('time measuring 2/2')){
 			l('date').innerHTML='Yearff '+(G.year+1)+' in '+G.getName('civ');
    			 G.addTooltip(l('date'),function(){return '<div class="barred">Date</div><div class="par">This is the current date in your civilization.<br>Sometime a new year starts. To see days obtain <b>Time measuring</b> 2/2 research.</div>';},{offY:-8});
 			    
-			    }if(G.has('time measuring 1/2') && G.has('time measuring 2/2')){
+			    }else if(G.has('time measuring 1/2') && G.has('time measuring 2/2')){
 			l('date').innerHTML='Year '+(G.year+1)+', day '+(G.day+1)+' in '+G.getName('civ');
    			 G.addTooltip(l('date'),function(){return '<div class="barred">Date</div><div class="par">This is the current date in your civilization.<br>One day elapses ....every second, and 300 days make up a year.</div>';},{offY:-8});
 			    
