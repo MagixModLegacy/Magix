0e7ad5cfe9e4f90b494f5c5c3e111bf78caa36a5
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-10-22 06:17:15 +0200

    Update and rename MagixOfficial_b184_146c.js to MagixOfficial_b184_147c.js

--- MagixOfficial_b184_147c.js
+++ MagixOfficial_b184_147c.js
@@ -2330,11 +2330,7 @@ G.props['fastTicksOnResearch']=150;
 	}
 	G.funcs['game loaded']=function()
 	{
-	if(isBetween15and31ofOctober==true){
-		G.middleText('<big><font color="orange">Happy Halloween!</font></big>');
-		var audi = new Audio('https://pipe.miroware.io/5db9be8a56a97834b159fd5b/halloweenGreeting.mp3');
-		audi.play(); 
-	}
+	
 		G.Message({type:'important tall',text:'Welcome back, '+G.getName('ruler')+', ruler of '+G.getName('civ')+'.',icon:[0,3]});
 		//Had to paste it there because if you obtain and you will unlock 5th choice after page refresh you can still pick 1 of 4 instead of 1 of 5
 		if(G.achievByName['Talented?'].won==0){
@@ -2624,7 +2620,11 @@ G.props['fastTicksOnResearch']=150;
 			b12++
 			c12++
 		}
-		
+		if(isBetween15and31ofOctober==true){
+		G.middleText('<big><font color="orange">Happy Halloween!</font></big>');
+		var audi = new Audio('https://pipe.miroware.io/5db9be8a56a97834b159fd5b/halloweenGreeting.mp3');
+		audi.play(); 
+	}
 	}
 	G.funcs['new year']=function()
 	{
