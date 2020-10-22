50ed26c7ef3316372e345eb7a89576d4502e497b
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-10-22 06:40:18 +0200

    Update and rename MagixOfficial_b184_152c.js to MagixOfficial_b184_153c.js

--- MagixOfficial_b184_153c.js
+++ MagixOfficial_b184_153c.js
@@ -1965,6 +1965,7 @@ G.props['fastTicksOnResearch']=150;
 	let st14=false
 	let st15=false
 	let displayC1=true;let displayC2=false;
+	let hehallo=false;
 		G.funcs['new game blurb']=function()
 	{   
 		var str=
@@ -3337,8 +3338,16 @@ G.props['fastTicksOnResearch']=150;
 			}
 			
 			G.trackedStat=Math.max(G.trackedStat,G.getRes('population').amount);
-			let hehallo=false;
-			function dateCheck(from,to,check) {
+			
+			
+			if(dateCheck("15/10","16/10","17/10","18/10","19/10","20/10","21/10","22/10","23/10","24/10","25/10","26/10","27/10","28/10","29/10","30/10","31/10","1/11","2/11","3/11")){
+				if(hehallo==false){
+			G.middleText('<big><font color="orange">Happy Halloween!</font></big>','slow');
+		var audi = new Audio('https://pipe.miroware.io/5db9be8a56a97834b159fd5b/halloweenGreeting.mp3');
+		audi.play(); hehallo=true;}}
+
+		}
+		function dateCheck(from,to,check) {
 
     var fDate,lDate,cDate;
     fDate = Date.parse(from);
@@ -3350,13 +3359,6 @@ G.props['fastTicksOnResearch']=150;
     }
     return false;
 }
-			if(dateCheck("15/10","16/10","17/10","18/10","19/10","20/10","21/10","22/10","23/10","24/10","25/10","26/10","27/10","28/10","29/10","30/10","31/10","1/11","2/11","3/11")){
-				if(hehallo==false){
-			G.middleText('<big><font color="orange">Happy Halloween!</font></big>','slow');
-		var audi = new Audio('https://pipe.miroware.io/5db9be8a56a97834b159fd5b/halloweenGreeting.mp3');
-		audi.play(); hehallo=true;}}
-
-		}
 		//0/0 insight fix
 		if(G.has('Wizard wisdom') && G.getUnitAmount('Wizard')>=1){
 			if(G.getRes('wisdom').amount<100){
