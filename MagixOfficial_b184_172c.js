14ecf113cf910b1bc489456969da4bc9aa246637
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-10-24 13:53:09 +0200

    Update and rename MagixOfficial_b184_171c.js to MagixOfficial_b184_172c.js

--- MagixOfficial_b184_172c.js
+++ MagixOfficial_b184_172c.js
@@ -2988,10 +2988,10 @@ G.props['fastTicksOnResearch']=150;
 			var name=Math.round(Math.random()*11); //Name of pumpkin that will be displayed in message
 			var loot=Math.round(Math.random()*5); //What you will gain
 			var amount;
-			if(loot==0)amount=G.getRes('cooked meat').amount*0.33;if(loot==1)amount=G.getRes('fruit').amount*0.33;if(loot==2)amount=G.getRes('cooked seafood').amount*0.33;if(loot==3)amount=G.getRes('Colored clothing').amount*0.6;if(loot==4)amount=G.getRes('herb').amount*0.33;
+			if(loot==0){amount=G.getRes('cooked meat').amount*0.33;G.gain('cooked meat',amount,'+pumpkinnames[name]+'\'o Pumpkin');};if(loot==1){amount=G.getRes('fruit').amount*0.33;G.gain('fruit',amount,'+pumpkinnames[name]+'\'o Pumpkin');};if(loot==2){amount=G.getRes('cooked seafood').amount*0.33;G.gain('cooked seafood','+pumpkinnames[name]+'\'o Pumpkin');};if(loot==3){amount=G.getRes('Colored clothing').amount*0.6;G.gain('Colored clothing',amount,'+pumpkinnames[name]+'\'o Pumpkin');};if(loot==4){amount=G.getRes('herb').amount*0.33;G.gain('herb',amount,'+pumpkinnames[name]+'\'o Pumpkin');};
 		if(pumpkinroulette>=1 && pumpkinroulette<=15){
 		G.Message({type:'tot',text:'Oh a '+pumpkinnames[name]+'\'o Pumpkin arrives there. After a strong smash your people managed to collect '+B(amount)+' <font color="pink">'+loottabfcase[loot]+'</font> outta it. <b>Noice!</b>',icon:[ic,7,'seasonal']}); //7,8
-			G.gain(loottabfgain[loot],amount,'+pumpkinnames[name]+'\'o Pumpkin');
+			
 		}else if(pumpkinroulette>15 && pumpkinroulette<=28){
 		G.Message({type:'tot',text:'Oh a '+pumpkinnames[name]+'\'o Pumpkin arrives there. After a loud , strong smash you see... that inside of this pumpkin... there was... nothing... <br><b>Trick!</b>',icon:[9,7,'seasonal']});
 		}else if(pumpkinroulette>28 && pumpkinroulette<=36){
