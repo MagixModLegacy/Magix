0dc34c5252ea3fd6f9b9d453e1d5781b6c1e9e09
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-11-13 16:05:15 +0100

    Update and rename MagixOfficial_b184_227c.js to MagixOfficial_b184_228c.js

--- MagixOfficial_b184_228c.js
+++ MagixOfficial_b184_228c.js
@@ -1690,6 +1690,33 @@ G.setPolicyMode=function(me,mode)
 			});
 		}
 	}
+	var trial11=function(){if(G.has('Eotm')){
+			G.button({id:'addBulk',
+					text:'<span style="position:relative;width:9px;margin-left:-4px;margin-right:-4px;z-index:10;font-weight:bold;">+</span>',
+					tooltip:'Multiply by 10',
+					onclick:function(){
+						var n=G.getSetting('buyAmount');
+						if (G.keys[17]) n=-n;
+						else
+						{
+							if (n==-1) n=1;
+							else if (n>-1) n=n*10;
+							else if (n<-1) n=n/10;
+						}
+						n=Math.round(n);
+						n=Math.max(Math.min(n,1e+35),-1e+35);
+						G.setSetting('buyAmount',n);
+						G.updateBuyAmount();
+						if (G.checkPolicy('Toggle SFX')=='on'){
+						var audio = new Audio('http://orteil.dashnet.org/cookieclicker/snd/press.mp3');
+						audio.play(); 
+						}
+					}
+				})+
+			}+
+	}else{
+			
+	}
 	///////////MORE QUOTES!
 	G.cantWhenPaused=function()
 	{
@@ -1749,30 +1776,7 @@ G.setPolicyMode=function(me,mode)
 						
 					},
 				})+
-			if(G.has('Eotm')){
-			G.button({id:'addBulk',
-					text:'<span style="position:relative;width:9px;margin-left:-4px;margin-right:-4px;z-index:10;font-weight:bold;">+</span>',
-					tooltip:'Multiply by 10',
-					onclick:function(){
-						var n=G.getSetting('buyAmount');
-						if (G.keys[17]) n=-n;
-						else
-						{
-							if (n==-1) n=1;
-							else if (n>-1) n=n*10;
-							else if (n<-1) n=n/10;
-						}
-						n=Math.round(n);
-						n=Math.max(Math.min(n,1e+35),-1e+35);
-						G.setSetting('buyAmount',n);
-						G.updateBuyAmount();
-						if (G.checkPolicy('Toggle SFX')=='on'){
-						var audio = new Audio('http://orteil.dashnet.org/cookieclicker/snd/press.mp3');
-						audio.play(); 
-						}
-					}
-				})+
-			}+
+				trial11();
 				'<div id="buyAmount" class="bgMid framed" style="width:128px;display:inline-block;padding-left:8px;padding-right:8px;font-weight:bold;">...</div>'+
 				G.button({id:'addBulk',
 					text:'<span style="position:relative;width:9px;margin-left:-4px;margin-right:-4px;z-index:10;font-weight:bold;">+</span>',
