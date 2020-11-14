5fb6bd83c55f08acb762312beabcd678a3fd8d09
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-11-14 08:25:57 +0100

    Update and rename MagixOfficial_b184_250c.js to MagixOfficial_b184_251c.js

--- MagixOfficial_b184_251c.js
+++ MagixOfficial_b184_251c.js
@@ -1762,7 +1762,7 @@ G.setPolicyMode=function(me,mode)
 						}
 					}
 				})+
-			'</center>'+</div></div>'+
+			'</center></div></div>'+
 			
 			'<div class="fullCenteredOuter" style="padding-top:16px;"><div id="unitBox" class="thingBox fullCenteredInner"></div></div>';
 		
