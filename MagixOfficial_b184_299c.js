455701fa704ba7e65b01116df8961c3bea237ed0
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-11-18 21:00:18 +0100

    Update and rename MagixOfficial_b184_298c.js to MagixOfficial_b184_299c.js

--- MagixOfficial_b184_299c.js
+++ MagixOfficial_b184_299c.js
@@ -1998,8 +1998,10 @@ G.props['fastTicksOnResearch']=150;
 	let st14=false
 	let st15=false
 	let displayC1=true;let displayC2=false;
-	let hehallo=false;
-	let dem=false
+	let hehallo=false
+	function democracy(){
+		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('influence'))+'"></div><div class="freelabel">x300</div>','300 Herbs')
+	};
 		G.funcs['new game blurb']=function()
 	{   
 		var str=
@@ -2009,7 +2011,7 @@ G.props['fastTicksOnResearch']=150;
 		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('elder'))+'"></div><div class="freelabel">x1</div>','1 Elder')+
 		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('child'))+'"></div><div class="freelabel">x2</div>','2 Children')+
 		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('herb'))+'"></div><div class="freelabel">x300</div>','300 Herbs')+
-		 (dem==true ? G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('influence'))+'"></div><div class="freelabel">x300</div>','300 Herbs'):"")+
+		 (G.achievByName['Democration'].won>=1 ? democracy() :"")+
 		G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('water'))+'"></div><div class="freelabel">x250</div>','250 Water')+
 		//G.textWithTooltip('<div class="icon freestanding" style="'+G.getIconUsedBy(G.getRes('insight'))+'"></div><div class="freelabel">x6</div>','6 Insight')+
 		'<script type="text/javascript">'+
