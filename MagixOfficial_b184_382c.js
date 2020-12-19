3948c4fed69a2c241be45a35613c13ef20949c6f
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2020-12-19 11:28:49 +0100

    Update and rename MagixOfficial_b184_381c.js to MagixOfficial_b184_382c.js

--- MagixOfficial_b184_382c.js
+++ MagixOfficial_b184_382c.js
@@ -715,7 +715,8 @@ if (!document.getElementById(cssId))
     link.rel  = 'stylesheet';
     link.type = 'text/css';
     link.href = 'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/beta.css';
-   link.href = 'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/snow.css';
+	link  = document.createElement('link');
+   link.href += 'https://pipe.miroware.io/5db9be8a56a97834b159fd5b/snow.css';
     link.media = 'all';
     head.appendChild(link);
 }
