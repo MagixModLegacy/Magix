b1bdbd792d1194c6c9f7b8b745420d71a2ab773e
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-01-14 14:37:21 +0100

    Update and rename MagixOfficial_b184_534c.js to MagixOfficial_b184_535c.js

--- MagixOfficial_b184_535c.js
+++ MagixOfficial_b184_535c.js
@@ -3316,7 +3316,7 @@ if (G.achievByName['Pocket'].won > 1 && G.hasNot('well stored 2')){
 		inscost=1*(G.getRes("New world point").amount/3)*((G.achievByName['Faithful'].won/2)+1);
 		
 	};
-	setInterval(function(){if(la>lb){l('mapSection').style.display='block';}else{l('mapSection').style.display='none';}},200);
+	setInterval(function(){if(la>lb && G.tab.id=='land'){l('mapSection').style.display='block';}else if(G.tab.id!='land'){l('mapSection').style.display='none';}},200);
 	G.funcs['tracked stat str c1']=function()
 	{
 		return 'Most population ruled';
