9e79464c1cc49bb050a528f9fc3a6589d6bee008
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-01-15 11:00:22 +0100

    Update and rename MagixOfficial_b184_541c.js to MagixOfficial_b184_542c.js

--- MagixOfficial_b184_542c.js
+++ MagixOfficial_b184_542c.js
@@ -17776,8 +17776,8 @@ new G.Tech({
 		],
 		req:{'tribalism':true,'policies':true},
 	});
-	setInterval(function(){if(G.has(la>lb)){
-				  if(G.tab.id=='land'){
+	setInterval(function(){if(la>lb)){
+				  if(G.tab.id=="land"){
 				G.showMap();
 			      }else if(G.tab.id!='land' || G.hasNot('where am i?')){G.hideMap();}
 	}},200);
