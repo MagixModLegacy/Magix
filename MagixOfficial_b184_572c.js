dc635deaa4802495155f0da61b51d6b166780ae9
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-01-18 09:20:10 +0100

    Update and rename MagixOfficial_b184_571c.js to MagixOfficial_b184_572c.js

--- MagixOfficial_b184_572c.js
+++ MagixOfficial_b184_572c.js
@@ -959,7 +959,7 @@ if (!document.getElementById(cssId))
 		}else{
 			var texts= ['All you know for now is that you exist at lands where scarce<br> not really occurs','All you know for now is that you exist in harsh, dry, <br>where food and water are scarce','You don\'t know where you are... You feel some warmth/cool , dryness/wettiness... Where?!'];
 			var str='';
-			str+='<div class="fullCenteredOuter"><div class="fullCenteredInner"><div class="barred fancyText"><center><font size="4">Get <font color="#7f7fff">Where am I?</font> trait to unlock content of this tab<br><li>There you\'ll see informations about land your tribe settled</li><br><li>Not only that. You\'ll also see which goods you can find at these lands</li><br><li>'+texts[G.achievByName[lc].won]+'</li><br><u>"The world is a book and those who don\'t travel read only one page"</u> ~ Saint Augustine</font></center></div></div><div id="techBox" class="thingBox"></div></div></div></div>';
+			str+='<div class="fullCenteredOuter"><div class="fullCenteredInner"><div class="barred fancyText"><center><font size="4">Get <font color="#7f7fff">Where am I?</font> trait to unlock content of this tab<br><li>There you\'ll see informations about land your tribe settled</li><br><li>Not only that. You\'ll also see which goods you can find at these lands</li><br><li>'+texts[lc]+'</li><br><u>"The world is a book and those who don\'t travel read only one page"</u> ~ Saint Augustine</font></center></div></div><div id="techBox" class="thingBox"></div></div></div></div>';
 			l('landDiv').innerHTML=str;
 			
 		
