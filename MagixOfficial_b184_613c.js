7b32ee307cf2cb51086c99cd9cfdab36fea4b09e
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-01-20 18:44:29 +0100

    Update and rename MagixOfficial_b184_612c.js to MagixOfficial_b184_613c.js

--- MagixOfficial_b184_613c.js
+++ MagixOfficial_b184_613c.js
@@ -1652,7 +1652,7 @@ G.setPolicyMode=function(me,mode)
 			var div=l('tech-'+me.id);if (div) me.l=div; else me.l=0;
 			var div=l('tech-icon-'+me.id);if (div) me.lIcon=div; else me.lIcon=0;
 			var div=l('tech-over-'+me.id);if (div) me.lOver=div; else me.lOver=0;
-			G.addTooltip(me.l,function(what){return function(){return G.getTechTooltip(what)};}(me.tech),{offY:-8});
+			G.addTooltip(me.l,function(what){return function(){return G.getKnowTooltip(what)};}(me.tech),{offY:-8});
 			if (me.l) me.l.onclick=function(what){return function(){G.clickTech(what);};}(me);
 		}
 		
