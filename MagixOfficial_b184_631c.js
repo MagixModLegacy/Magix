64ddb441ff99638c5e404b17b1b2d008978f792d
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-01-21 07:08:58 +0100

    Update and rename MagixOfficial_b184_630c.js to MagixOfficial_b184_631c.js

--- MagixOfficial_b184_631c.js
+++ MagixOfficial_b184_631c.js
@@ -2532,7 +2532,7 @@ G.update.tech = () => {
   '?',
   `<div style="width:240px;text-align:left;"><div class="par">Technologies are the cornerstone of your civilization's long-term development.</div><div class="par">Here you can invest resources to research new technologies which can unlock new units and enhance old ones.</div></div>`,
   'infoButton',
- )}<div class="fullCenteredOuter"><div class="fullCenteredInner"><div id="extraTechStuff" style="text-align:center;margin:auto;margin-bottom:8px;width:600px;"><div class="barred fancyText" height="85px"><font size="6">Known technologies :</font></div></div><div id="techBox" class="thingBox"></div></div></div></div><div id="techUI" class="bottomUI bgPanelUp">${G.writeChooseBoxes(
+ )}<div class="fullCenteredOuter"><div class="fullCenteredInner"><div id="extraTechStuff" style="text-align:center;margin:auto;margin-bottom:8px;width:600px;"><font size="6">Known technologies :</font></div></div><div id="techBox" class="thingBox"></div></div></div></div><div id="techUI" class="bottomUI bgPanelUp">${G.writeChooseBoxes(
   'tech',
  )}</div>`;
  l('techDiv').innerHTML = researchUI;
