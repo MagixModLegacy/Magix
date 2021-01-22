2c917b8cb6d999aacbd4ee79c6ad72e747318253
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-01-22 19:37:54 +0100

    Update and rename MagixOfficial_b184_653c.js to MagixOfficial_b184_654c.js

--- MagixOfficial_b184_654c.js
+++ MagixOfficial_b184_654c.js
@@ -2531,6 +2531,7 @@ G.update.tech = () => {
   '?',
   `<div style="width:240px;text-align:left;"><div class="par">Technologies are the cornerstone of your civilization's long-term development.</div><div class="par">Here you can invest resources to research new technologies which can unlock new units and enhance old ones.</div></div>`,
   'infoButton',
+	 '<p style="float:right">Thanks to Hyoretsu for helping with coding this tab</p>',
  )}<div class="fullCenteredOuter"><div class="fullCenteredInner"><div id="extraTechStuff" style="text-align:center;margin:auto;margin-bottom:8px;"><div class="barred fancyText"><font size="6">Known technologies :</font></div></div><div id="techBox" class="thingBox"></div></div></div></div><div id="techUI" class="bottomUI bgPanelUp">${G.writeChooseBoxes(
   'tech',
  )}</div>`;
