5fe4edb76b916dd44cce5dc4ba95864423392c09
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-01-24 14:50:21 +0100

    Update and rename MagixOfficial_b184_687c.js to MagixOfficial_b184_688c.js

--- MagixOfficial_b184_688c.js
+++ MagixOfficial_b184_688c.js
@@ -20327,5 +20327,298 @@ var len=G.techsOwned.length;
 	
   G.draw.tech();
  });
+G.funcs['create map']=function(w,h)
+	{
+		//generate basic geography using Conway's Game of Life (rule : births from 4 to 9 neighbors, survival from 6 to 9 neighbors)
+		
+		var generate=function(w,h)
+		{
+			var getAt=function(map,x,y)
+			{
+				//if (x<0||x>=map.length||y<0||y>=map[0].length) return 0;
+				//wrap around so we don't get big empty spots on the edges (as a bonus, this creates donut-shaped worlds)
+				if (x<0) x+=map.length;
+				else if (x>=map.length) x-=map.length;
+				if (y<0) y+=map[0].length;
+				else if (y>=map[0].length) y-=map[0].length;
+				return map[x][y];
+			}
+			
+			//init map
+			var lvl=[];
+			for (var x=0;x<w;x++)
+			{
+				lvl[x]=[];
+				for (var y=0;y<h;y++)
+				{
+					lvl[x][y]=Math.random()<0.5?1:0;
+				}
+			}
+			
+			//init buffer
+			var lvlBuffer=[];
+			for (var x=0;x<w;x++){lvlBuffer[x]=[];for (var y=0;y<h;y++){lvlBuffer[x][y]=0;}}
+			
+			var passes=1;
+			for (var i=0;i<passes;i++)
+			{
+				//live
+				for (var x=0;x<w;x++)
+				{
+					for (var y=0;y<h;y++)
+					{
+						var n=getAt(lvl,x-1,y)+getAt(lvl,x-1,y-1)+getAt(lvl,x,y-1)+getAt(lvl,x+1,y-1)+getAt(lvl,x+1,y)+getAt(lvl,x+1,y+1)+getAt(lvl,x,y+1)+getAt(lvl,x-1,y+1);
+						var on=lvl[x][y];
+						if (on && n>=4 && n<=9) on=1; else on=0;
+						if (!on && n>=6 && n<=9) on=1;
+						if (Math.random()<0.05) on=Math.random()<0.5?1:0;//just a bit of extra randomness
+						lvlBuffer[x][y]=on;
+					}
+				}
+				//copy buffer back
+				for (var x=0;x<w;x++){for (var y=0;y<h;y++){lvl[x][y]=lvlBuffer[x][y];}}
+			}
+			
+			return lvl;
+		}
+		
+		var getStrAt=function(map,x,y)
+		{
+			if (x<0||x>=map.length-1||y<0||y>=map[0].length-1) return 'out';
+			return map[x][y];
+		}
+		var getAt=function(map,x,y)
+		{
+			if (x<0||x>=map.length-1||y<0||y>=map[0].length-1) return 0.5;
+			return map[x][y];
+		}
+		
+		var landTiles=[];
+		var seaTiles=[];
+		var fit=false;
+		i=0;
+		while (i<20 && fit==false)//discard any map with less than 30% or more than 50% land
+		{
+			var lvl=generate(w,h);
+			
+			landTiles=[];
+			seaTiles=[];
+			for (var x=0;x<w;x++)
+			{
+				for (var y=0;y<h;y++)
+				{
+					if (lvl[x][y]==0) seaTiles.push([x,y]);
+					else landTiles.push([x,y]);
+				}
+			}
+			var total=landTiles.length+seaTiles.length;
+			if (landTiles.length/total>0.3 && landTiles.length/total<0.5) fit=true;
+			i++;
+		}
+		
+		//translate into terrain
+		for (var x=0;x<w;x++)
+		{
+			for (var y=0;y<h;y++)
+			{
+				var land='ocean';
+				if (lvl[x][y]==0) land='ocean';
+				else if (lvl[x][y]==1)
+				{
+					land='none';
+				}
+				lvl[x][y]=land;
+			}
+		}
+		
+		//precipitation map
+		//generate more humidity over sea, less in land - with some variance
+		//on tiles with low humidity, 30% of the time, add some huge variance
+		//then, blur the map so that coasts get some humidity and variance can spread
+		var wet=[];
+		for (var x=0;x<w;x++)
+		{
+			wet[x]=[];
+			for (var y=0;y<h;y++)
+			{
+				wet[x][y]=(lvl[x][y]=='ocean'?0.8:0.2)+Math.random()*0.1-0.1/2;
+				if (Math.random()<0.3 && wet[x][y]<0.5) wet[x][y]+=Math.random()*5-2.5;
+			}
+		}
+		for (var x=0;x<w;x++)//blur
+		{
+			for (var y=0;y<h;y++)
+			{
+				var variance=0.05;
+				var n=getAt(wet,x-1,y)+getAt(wet,x-1,y-1)+getAt(wet,x,y-1)+getAt(wet,x+1,y-1)+getAt(wet,x+1,y)+getAt(wet,x+1,y+1)+getAt(wet,x,y+1)+getAt(wet,x-1,y+1);
+				wet[x][y]=(wet[x][y]+n)/9+Math.random()*variance-variance/2;
+			}
+		}
+		//temperature map. why not
+		var jumble=false;
+		if (!jumble)
+		{
+			//vertical sine wave (so we get hot equator and cold poles), with some variance
+			//humidity lowers temperature by a bit
+			var temp=[];
+			for (var x=0;x<w;x++)
+			{
+				temp[x]=[];
+				for (var y=0;y<h;y++)
+				{
+					var variance=0.15;
+					temp[x][y]=Math.sin(((y+0.5)/h-0.25)*Math.PI*2)/2+(lvl[x][y]=='ocean'?0.6:0.5)-(wet[x][y])*0.15+Math.random()*variance-variance/2;
+				}
+			}
+		}
+		else
+		{
+			//temperature spawns in big blobs of cold and hot
+			var temp=[];
+			for (var x=0;x<w;x++)
+			{
+				temp[x]=[];
+				for (var y=0;y<h;y++)
+				{
+					temp[x][y]=0.65+Math.random()*0.1-0.1/2-wet[x][y]*0.15;
+					if (Math.random()<0.5) temp[x][y]+=Math.random()*10-5;
+				}
+			}
+			for (var i=0;i<2;i++)//blur
+			{
+				for (var x=0;x<w;x++)
+				{
+					for (var y=0;y<h;y++)
+					{
+						var variance=0.05;
+						var n=getAt(temp,x-1,y)+getAt(temp,x-1,y-1)+getAt(temp,x,y-1)+getAt(temp,x+1,y-1)+getAt(temp,x+1,y)+getAt(temp,x+1,y+1)+getAt(temp,x,y+1)+getAt(temp,x-1,y+1);
+						temp[x][y]=(temp[x][y]+n)/9+Math.random()*variance-variance/2;
+					}
+				}
+			}
+		}
+		
+		//biomes
+		for (var x=0;x<w;x++)
+		{
+			for (var y=0;y<h;y++)
+			{
+				var tempTile=temp[x][y];
+				var wetTile=wet[x][y];
+				var landTile=lvl[x][y];
+				
+				var biomes=[];
+				if (tempTile<-0.1275)
+				{
+					biomes.push('glacier');
+				}
+				else if (tempTile<-0.1 && tempTile>-0.1275)
+				{
+					if (landTile=='ocean') biomes.push('arctic ocean');
+					else biomes.push('ice desert');
+				}
+				else if (tempTile<0.15)
+				{
+					if (landTile=='ocean') biomes.push('arctic ocean');
+					else if (wetTile<0.25) biomes.push('ice desert');
+					else if (wetTile>0.5 && wetTile<0.75) biomes.push('boreal forest');
+					else if (wetTile>0.75) biomes.push('swamplands');
+					else biomes.push('tundra');
+				}
+				else if (tempTile>1.1)
+				{
+					if (landTile=='ocean') biomes.push('tropical ocean');
+					else if(wetTile>0.04) biomes.push('xeric shrubland');
+					else biomes.push('desert');
+				}
+				else if (tempTile>0.85)
+				{
+					if (landTile=='ocean') biomes.push('tropical ocean');
+					else if (wetTile<=0.12) biomes.push('badlands');
+					else if (wetTile<0.25 && wetTile>0.18) biomes.push('desert');
+					else if(wetTile>0.3 && wetTile<0.385) biomes.push('xeric shrubland');
+					else if (wetTile>0.5 && wetTile <0.75) biomes.push('jungle');
+					else if (wetTile>0.884) biomes.push('dead forest');
+					else biomes.push('savanna');
+				}
+				else
+				{
+					if (landTile=='ocean') biomes.push('ocean');
+					else if (wetTile<0.25) biomes.push('shrubland');
+					else if (wetTile>0.5 && wetTile<0.78) biomes.push('forest');
+					else if (wetTile>0.78) biomes.push('lavender fields');
+					else biomes.push('prairie');
+				}
+				if (biomes.length==0) biomes.push('prairie');
+				lvl[x][y]=choose(biomes);
+			}
+		}
+		
+		for (var x=0;x<w;x++)//clean all tiles with no terrain
+		{
+			for (var y=0;y<h;y++)
+			{
+				if (lvl[x][y]=='none') lvl[x][y]='forest';
+			}
+		}
+		return lvl;
+    }}
+	G.update.tech = () => {
+ // Add background UI for Research
+ const researchUI = `<p style="float:right;">Thanks to Hyoretsu for helping with coding this tab</p><div class="behindBottomUI">${G.textWithTooltip(
+  '?',
+  `<div style="width:240px;text-align:left;"><div class="par">Technologies are the cornerstone of your civilization's long-term development.</div><div class="par">Here you can invest resources to research new technologies which can unlock new units and enhance old ones.</div></div>`,
+  'infoButton',
+ )}<div class="fullCenteredOuter"><div class="fullCenteredInner"><div id="extraTechStuff" style="text-align:center;margin:auto;margin-bottom:8px;"><div class="barred fancyText"><font size="6">Known technologies :</font></div></div><div id="techBox" class="thingBox"></div></div></div></div><div id="techUI" class="bottomUI bgPanelUp">${G.writeChooseBoxes(
+  'tech',
+ )}</div>`;
+ l('techDiv').innerHTML = researchUI;
+ G.addCallbacks();
+
+ const knowledgeCategories = {};
+ G.knowCategories.forEach(category => {
+  Object.assign(knowledgeCategories, { [category.id]: category.name });
+ });
+
+ G.techsOwned.forEach(techResearched => {
+  let category = l(`know-catName-${techResearched.tech.category}`);
+
+  // Create category if it doesn't yet exist
+  if (!category) {
+   const categoryName = techResearched.tech.category;
+   const categoryUI = `<div class="category" style="display:inline-block;"><div class="categoryName barred fancyText" id="know-catName-${categoryName}">${knowledgeCategories[categoryName]}</div></div>`;
+
+   l('techBox').innerHTML += categoryUI;
+  }
+  category = l(`know-catName-${techResearched.tech.category}`);
+
+  // Populate research
+  category.insertAdjacentHTML(
+   'afterend',
+   `<div class="thingWrapper"><div class="tech thing${G.getIconClasses(techResearched.tech)}" id="tech-${
+    techResearched.id
+   }">${G.getIconStr(techResearched.tech, `tech-icon-${techResearched.id}`)}<div class="overlay" id="tech-over-${
+    techResearched.id
+   }"></div></div></div>`,
+  );
+var len=G.techsOwned.length;
+            for (var i=0;i<len;i++)
+            {
+                var me=G.techsOwned[i];
+                var div=l('tech-'+me.id);if (div) me.l=div; else me.l=0;
+                var div=l('tech-icon-'+me.id);if (div) me.lIcon=div; else me.lIcon=0;
+                var div=l('tech-over-'+me.id);if (div) me.lOver=div; else me.lOver=0;
+                G.addTooltip(me.l,function(what){return function(){return G.getKnowTooltip(what)};}(me.tech),{offY:-8});
+                if (me.l) me.l.onclick=function(what){return function(){G.clickTech(what);};}(me);
+            }
+ /* techResearched.l = l(`tech-${techResearched.id}`);
+  techResearched.l.onclick = G.clickTech(techResearched);
+  techResearched.lIcon = l(`tech-icon-${techResearched.id}`);
+  techResearched.lOver = l(`tech-over-${techResearched.id}`);
+  // Add tooltip manually
+  G.addTooltip(techResearched.l, () => G.getKnowTooltip(techResearched), { offY: -8 });*/
+	
+  G.draw.tech();
+ });
 };
 });
