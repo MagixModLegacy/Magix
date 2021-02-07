cc43585f4dc67551d85214191b6aad5fe30ab1a4
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-02-07 12:55:17 +0100

    Update and rename MagixOfficial_b184_740c.js to MagixOfficial_b184_741c.js

--- MagixOfficial_b184_741c.js
+++ MagixOfficial_b184_741c.js
@@ -9610,7 +9610,7 @@ if (!document.getElementById(cssId))
 		icon:[10,16,'seasonal'],
 		tick:function(me,tick)
 		{
-			var xpreq=(((G.getRes('love').amount+10)*55000)^G.getRes('love').amount)*G.getRes('love').amount;
+			var xpreq=(((G.getRes('love').amount+10)*55000)^G.getRes('love').amount)*G.getRes('love').amount+3*(G.getRes('love').amount/3);
 			if(G.getRes('love xp').amount>=xpreq){
 			G.getRes('love xp').amount=0;me.amount++;	
 			}
@@ -9619,7 +9619,7 @@ if (!document.getElementById(cssId))
 		{
 			if (G.getRes('population').amount<=0) return '<b>-</b>';
 			var amount=G.getRes('love').amount;
-			var xpreq=(((G.getRes('love').amount+10)*55000)^G.getRes('love').amount)*G.getRes('love').amount;
+			var xpreq=(((G.getRes('love').amount+10)*55000)^G.getRes('love').amount)*G.getRes('love').amount+3*(G.getRes('love').amount/3);
 			return '<font color="yellow"><b>Lvl '+B(amount)+'</b></font> '+B(G.getRes('love xp').amount)+'/<br>'+B(xpreq)+'';
 		},
 		getIcon:function(me)
