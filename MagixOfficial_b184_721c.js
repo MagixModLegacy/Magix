961529a2064f9b4b1263c37eb1853ffd0d111297
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-02-07 10:56:15 +0100

    Update and rename MagixOfficial_b184_720c.js to MagixOfficial_b184_721c.js

--- MagixOfficial_b184_721c.js
+++ MagixOfficial_b184_721c.js
@@ -9619,7 +9619,7 @@ if (!document.getElementById(cssId))
 		{
 			if (G.getRes('population').amount<=0) return '<b>-</b>';
 			var amount=G.getRes('love').amount;
-			var xpreq=(G.getRes('love').amount^1/2)*35000/(G.techN/100)+(G.trait.length*100);
+			var xpreq=(G.getRes('love').amount^3/2)*35000/(G.techN/100)+(G.trait.length*100);
 			return '<font size="1"><font color="yellow"><b> Lvl '+B(amount)+'</b></font> '+B(G.getRes('love xp').amount)+'/<br>'+B(xpreq)+'</font>';
 		},
 		getIcon:function(me)
@@ -9627,7 +9627,7 @@ if (!document.getElementById(cssId))
 			if (G.getRes('population').amount<=0) return [17,16,'seasonal'];
 			else
 			{
-				var amount=G.getRes('population').amount;
+				var amount=G.getRes('love').amount;
 				if (amount>=15) return [16,16,'seasonal'];
 				else if (amount>=12) return [15,16,'seasonal'];
 				else if (amount>=10) return [14,16,'seasonal'];
