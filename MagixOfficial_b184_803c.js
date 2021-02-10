bc3a091e7d68000fc9ae04af41eabda084d0a234
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-02-10 09:48:08 +0100

    Update and rename MagixOfficial_b184_802c.js to MagixOfficial_b184_803c.js

--- MagixOfficial_b184_803c.js
+++ MagixOfficial_b184_803c.js
@@ -2067,7 +2067,7 @@ G.setPolicyMode=function(me,mode)
 						G.dialogue.getCloseButton('- Back -')+
 						'</div>';
 					}
-					else if(me.name=="scientific university" || me.name='fortress of love' || me.name='wonderful fortress of christmas')
+					else if(me.name=="scientific university" || me.name=='fortress of love' || me.name=='wonderful fortress of christmas')
 					{
 						str+='<div class="fancyText par">Wonder completed</div>';
 						str+='<div class="fancyText par">You cannot ascend by this wonder. Not every wonder means ascension and here is example of that.</div>';
