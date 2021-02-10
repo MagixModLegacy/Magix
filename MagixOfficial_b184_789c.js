e04ca36c5d4443ede61a4f7ae1e39a5849b13108
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-02-10 07:11:42 +0100

    Update and rename MagixOfficial_b184_788c.js to MagixOfficial_b184_789c.js

--- MagixOfficial_b184_789c.js
+++ MagixOfficial_b184_789c.js
@@ -9704,11 +9704,10 @@ if (!document.getElementById(cssId))
 	new G.Res({
 		name:'love',
 		desc:'Seasonal essential. //[love] can unlock new bonuses on some levels. //Obtaining valentine traits will allow you to gain points for that essential from more sources. Depending on happiness you may gain it slower or faster. You won\'t gain any more [love] if [happiness] level will go below -100%. Xp gains will be lowered by 75% if your [health] level will go below -20%.',
-		startWith:1,
 		icon:[10,16,'seasonal'],
 		tick:function(me,tick)
 		{
-			var xpreq=((((G.getRes('love').amount+10)*55000)^G.getRes('love').amount)*G.getRes('love').amount+3)*(G.getRes('love').amount/1.85);
+			var xpreq=((((G.getRes('love').amount+10)*55000)^G.getRes('love').amount)*G.getRes('love').amount+3)*((G.getRes('love').amount+1)/1.85);
 			if(G.getRes('love xp').amount>=xpreq){
 			G.getRes('love xp').amount=0;me.amount++;	
 			}
@@ -9717,8 +9716,8 @@ if (!document.getElementById(cssId))
 		{
 			if (G.getRes('population').amount<=0) return '<b>-</b>';
 			var amount=G.getRes('love').amount;
-			var xpreq=((((G.getRes('love').amount+10)*55000)^G.getRes('love').amount)*G.getRes('love').amount+3)*(G.getRes('love').amount/1.85);
-			return '<font color="yellow"><b>Lvl '+B(amount)+'</b></font> '+B(G.getRes('love xp').amount)+'/<br>'+B(xpreq)+'';
+			var xpreq=((((G.getRes('love').amount+10)*55000)^G.getRes('love').amount)*G.getRes('love').amount+3)*((G.getRes('love').amount+1)/1.85);
+			return '<font color="yellow"><b>Lvl '+B(amount+1)+'</b></font> '+B(G.getRes('love xp').amount)+'/<br>'+B(xpreq)+'';
 		},
 		getIcon:function(me)
 		{
