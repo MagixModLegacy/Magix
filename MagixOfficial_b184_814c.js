48b7db0ebfee562a29bb02fdeb1aecb223c3de5d
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-02-23 18:45:30 +0100

    Update MagixOfficial_b184_814c.js

--- MagixOfficial_b184_814c.js
+++ MagixOfficial_b184_814c.js
@@ -2846,6 +2846,7 @@ G.props['fastTicksOnResearch']=150;
 	//////////////////////////////////////
 	G.funcs['new game']=function()
 	{
+		document.title='NeverEnding Legacy';
 		///new game mesg
 		var str='Your name is '+G.getName('ruler')+''+((G.getName('ruler').toLowerCase()=='orteil' || G.getName('ruler').toLowerCase()=='pelletsstarpl' || G.getName('ruler').toLowerCase()=='opti' )?' <i>(but that\'s not you, is it?)</i>':'')+', ruler of '+G.getName('civ')+'. Your tribe is primitive, but full of hope.<br>The first year of your legacy has begun. May it stand the test of time.';
 		G.Message({type:'important tall',text:str,icon:[0,3]});
