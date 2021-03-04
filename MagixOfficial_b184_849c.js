67c77b33043d94a43bccff9c008050d31aa6abfd
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-03-04 09:15:29 +0100

    Update and rename MagixOfficial_b184_848c.js to MagixOfficial_b184_849c.js

--- MagixOfficial_b184_849c.js
+++ MagixOfficial_b184_849c.js
@@ -713,7 +713,7 @@ func:function(){
 	var easterDay=function(Y){var C = Math.floor(Y/100);var N = Y - 19*Math.floor(Y/19);var K = Math.floor((C - 17)/25);var I = C - Math.floor(C/4) - Math.floor((C - K)/3) + 19*N + 15;I = I - 30*Math.floor((I/30));I = I - Math.floor(I/28)*(1 - Math.floor(I/28)*Math.floor(29/(I + 1))*Math.floor((21 - N)/11));var J = Y + Math.floor(Y/4) + I + 2 - C + Math.floor(C/4);J = J - 7*Math.floor(J/7);var L = I - J;var M = 3 + Math.floor((L + 40)/44);var D = L + 28 - 31*Math.floor(M/4);return new Date(Y,M-1,D);}(year);
 	easterDay=Math.floor((easterDay-new Date(easterDay.getFullYear(),0,0))/(1000*60*60*24));
 	/////////
-	
+	str+=G.getSafeName('patron')+';'; //NOT RELATED TO PATREON OR STH
 	//READ THIS: All rights reserved to mod creator and people that were helping the main creator with coding. Mod creator rejects law to copying icons from icon sheets used for this mod. All noticed plagiariasm will be punished. Copyright: 2020
 //===========================
 	/*=====================================================================================
