2c3ab51962f97cabca73cd34315840528cc40f98
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2021-03-06 20:02:41 +0100

    Update and rename MagixOfficial_b184_883c.js to MagixOfficial_b184_884c.js
    Removal of G.save and G.load custom declaration.
    Patrons name changed.

--- MagixOfficial_b184_884c.js
+++ MagixOfficial_b184_884c.js
@@ -727,449 +727,7 @@ func:function(){
 	var easterDay=function(Y){var C = Math.floor(Y/100);var N = Y - 19*Math.floor(Y/19);var K = Math.floor((C - 17)/25);var I = C - Math.floor(C/4) - Math.floor((C - K)/3) + 19*N + 15;I = I - 30*Math.floor((I/30));I = I - Math.floor(I/28)*(1 - Math.floor(I/28)*Math.floor(29/(I + 1))*Math.floor((21 - N)/11));var J = Y + Math.floor(Y/4) + I + 2 - C + Math.floor(C/4);J = J - 7*Math.floor(J/7);var L = I - J;var M = 3 + Math.floor((L + 40)/44);var D = L + 28 - 31*Math.floor(M/4);return new Date(Y,M-1,D);}(year);
 	easterDay=Math.floor((easterDay-new Date(easterDay.getFullYear(),0,0))/(1000*60*60*24));
 
-	G.Save=function(toStr)
-	{
-		//if toStr is true, don't actually save; return a string containing the save
-		if (!toStr && G.local && G.isIE) return false;
-		var str='';
-		
-		//general
-		G.lastDate=parseInt(Date.now());
-		str+=
-			parseFloat(G.engineVersion).toString()+';'+
-			parseFloat(G.startDate).toString()+';'+
-			parseFloat(G.fullDate).toString()+';'+
-			parseFloat(G.lastDate).toString()+';'+
-			parseFloat(G.year).toString()+';'+
-			parseFloat(G.day).toString()+';'+
-			parseFloat(G.fastTicks).toString()+';'+
-			parseFloat(G.furthestDay).toString()+';'+
-			parseFloat(G.totalDays).toString()+';'+
-			parseFloat(G.resets).toString()+';'+
-			'';
-		str+='|';
-		
-		//settings
-		for (var i in G.settings)
-		{
-			var me=G.settings[i];
-			if (me.type=='toggle') str+=(me.value?'1':'0');
-			else if (me.type=='int') str+=parseInt(me.value).toString();
-			str+=';';
-		}
-		str+='|';
-		
-		//mods
-		for (var i in G.mods)
-		{
-			var me=G.mods[i];
-			str+='"'+me.url.replaceAll('"','&quot;')+'":';
-			if (me.achievs)
-			{
-				//we save achievements separately for each mod
-				for (var ii in me.achievs)
-				{
-					str+=parseInt(me.achievs[ii].won).toString()+',';
-				}
-			}
-			str+=':';
-			//tracked stats (not fully implemented yet)
-			str+=parseFloat(G.trackedStat).toString();
-			str+=';';
-		}
-		str+='|';
-		
-		//culture and names
-		str+=(G.cultureSeed)+';';
-		str+=G.getSafeName('ruler')+';';
-		str+=G.getSafeName('civ')+';';
-		str+=G.getSafeName('civadj')+';';
-		str+=G.getSafeName('inhab')+';';
-		str+=G.getSafeName('inhabs')+';';
-		str+=G.getSafeName('patron')+';';
-		str+='|';
-		
-		//maps
-		str+=(G.currentMap.seed)+';';
-		
-		var map=G.currentMap;
-		for (var x=0;x<map.w;x++)
-		{
-			for (var y=0;y<map.h;y++)
-			{
-				var tile=map.tiles[x][y];
-				str+=
-					parseInt(tile.owner).toString()+':'+
-					parseInt(Math.floor(tile.explored*100)).toString()+':'+
-					',';
-			}
-		}
-		
-		str+='|';
-		
-		//techs & traits
-		var len=G.techsOwned.length;
-		for (var i=0;i<len;i++)
-		{
-			str+=parseInt(G.techsOwned[i].tech.id).toString()+';';
-		}
-		str+='|';
-		var len=G.traitsOwned.length;
-		for (var i=0;i<len;i++)
-		{
-			str+=parseInt(G.traitsOwned[i].trait.id).toString()+';';
-		}
-		str+='|';
-		
-		//policies
-		var len=G.policy.length;
-		for (var i=0;i<len;i++)
-		{
-			var me=G.policy[i];
-			if (me.visible)
-			{
-				str+=parseInt(me.id).toString()+','+parseInt(me.mode?me.mode.num:0).toString()+';';
-			}
-		}
-		str+='|';
-		
-		//res
-		var len=G.res.length;
-		for (var i=0;i<len;i++)
-		{
-			var me=G.res[i];
-			str+=
-				(!me.meta?(parseFloat(Math.round(me.amount)).toString()+','):'')+
-				(me.displayUsed?(parseFloat(Math.round(me.used)).toString()+','):'')+
-				(me.visible?'1':'0')+';';
-		}
-		str+='|';
-		
-		//units
-		var len=G.unitsOwned.length;
-		for (var i=0;i<len;i++)
-		{
-			var me=G.unitsOwned[i];
-			if (true)//me.amount>0)
-			{
-				str+=parseInt(me.unit.id).toString()+','+
-				parseFloat(Math.round(me.amount)).toString()+
-				((me.unit.gizmos||me.unit.wonder)?
-					(','+parseInt(me.unit.wonder?me.mode:(me.mode?me.mode.num:0)).toString()+','+//mode
-					parseInt(me.percent).toString())//percent
-					:'')+
-				','+parseFloat(Math.round(me.targetAmount)).toString()+
-				','+parseFloat(Math.round(me.idle)).toString()+
-				';';
-			}
-		}
-		str+='|';
-		
-		//chooseboxes
-		var len=G.chooseBox.length;
-		for (var i=0;i<len;i++)
-		{
-			var me=G.chooseBox[i];
-			var choices=[parseFloat(me.roll)];
-			for (var ii in me.choices)
-			{
-				choices.push(parseInt(me.choices[ii].id));
-			}
-			str+=choices.join(',')+';';
-		}
-		str+='|';
-		
-		//console.log('SAVE');
-		//console.log(str);
-		str=escape(str);
-		str=b64EncodeUnicode(str);
-		//console.log(Math.ceil(byteCount(str)/1000)+'kb');
-		if (!toStr)
-		{
-			window.localStorage.setItem(G.saveTo,str);
-			G.middleText('<font color="fuschia">- Game saved -</font>');
-			//console.log('Game saved successfully.');
-		}
-		else return str;
-	}
-	G.Load=function(doneLoading)
-	{
-		if (G.importStr) {var local=G.importStr;}
-		else
-		{
-			if (G.local && G.isIE) return false;
-			if (!window.localStorage) return false;
-			var local=window.localStorage.getItem(G.saveTo);
-		}
-		if (!local) return false;
-		var str='';
-		str=b64DecodeUnicode(local);
-		//console.log('LOAD');
-		//console.log(Math.ceil(byteCount(str)/1000)+'kb');
-		str=unescape(str);
-		//console.log(str);
-		if (str!='null' && str!='')
-		{
-			G.Reset();
-			G.resetSettings();
-			
-			//take care of strings first
-			G.stringsLoadedN=0;
-			G.stringsLoaded=[];
-			str=str.replace(/"(.*?)"/gi,G.parseLoadStrings);
-			
-			str=str.split('|');
-			
-			var s=0;
-			//general
-			var spl=str[s++].split(';');
-			//console.log('General : '+spl);
-			var i=0;
-			var fromVersion=parseFloat(spl[i++]);
-			G.startDate=parseFloat(spl[i++]);
-			G.fullDate=parseFloat(spl[i++]);
-			G.lastDate=parseFloat(spl[i++]);
-			G.year=parseFloat(spl[i++]);
-			G.day=parseFloat(spl[i++]);
-			G.fastTicks=parseFloat(spl[i++]);
-			G.furthestDay=parseFloat(spl[i++]);
-			G.totalDays=parseFloat(spl[i++]);
-			G.resets=parseFloat(spl[i++]);
-			//accumulate fast ticks when offline
-			var timeOffline=Math.max(0,(Date.now()-G.lastDate)/1000);
-			G.fastTicks+=Math.floor(timeOffline);
-			G.nextFastTick=Math.ceil((1-(timeOffline-Math.floor(timeOffline)))*G.tickDuration);
-			
-			//settings
-			var spl=str[s++].split(';');
-			//console.log('Settings : '+spl);
-			var len=spl.length;
-			for (var i=0;i<len;i++)
-			{
-				if (spl[i]!='' && G.settings[i])
-				{
-					var me=G.settings[i];
-					if (me.type=='toggle') me.value=(spl[i]=='1'?true:false);
-					else if (me.type=='int') me.value=parseInt(spl[i]);
-				}
-			}
-			for (var i in G.settings)
-			{
-				var me=G.settings[i];
-				if (me.onChange) me.onChange();
-			}
-			
-			
-			if (!doneLoading)
-			{
-				//mods
-				var spl=str[s++].split(';');
-				var mods=[];
-				for (var i in spl)
-				{
-					var spl2=spl[i].split(':');
-					var val=G.readLoadedString(spl2[0]);
-					if (val)
-					{
-						mods.push(val.replaceAll('&quot;','"'));
-					}
-				}
-				G.LoadMods(mods,G.Load,false);
-				return 1;
-			}
-			
-			G.importStr=0;
-			
-			//mod achievs & tracked stats
-			var spl=str[s++].split(';');
-			for (var i in spl)
-			{
-				var spl2=spl[i].split(':');
-				var mod=G.mods[i];
-				if (spl2[1] && mod.achievs)
-				{
-					bit=spl2[1].split(',');
-					for (var ii in bit)
-					{
-						if (bit[ii])
-						{
-							if (mod.achievs[ii]) mod.achievs[ii].won=parseInt(bit[ii]);
-						}
-					}
-				}
-				if (spl2[2])
-				{
-					bit=spl2[2].split(',');
-					for (var ii in bit)
-					{
-						if (bit[ii])
-						{
-							G.trackedStat=parseFloat(bit[ii]);
-						}
-					}
-				}
-			}
-			
-			//culture and names
-			var spl=str[s++].split(';');
-			var ss=0;
-			G.cultureSeed=spl[ss++];
-			G.setSafeName('ruler',G.readLoadedString(spl[ss++]),'Anonymous');
-			G.setSafeName('civ',G.readLoadedString(spl[ss++]),'nameless tribe');
-			G.setSafeName('civadj',G.readLoadedString(spl[ss++]),'tribal');
-			G.setSafeName('inhab',G.readLoadedString(spl[ss++]),'inhabitant');
-			G.setSafeName('inhabs',G.readLoadedString(spl[ss++]),'inhabitants');
-			G.setSafeName('patron',G.readLoadedString(spl[ss++]),'nameless patron');
-			//maps
-			var spl=str[s++].split(';');
-			//console.log('Map tiles : '+spl);
-			G.currentMap=new G.Map(0,24,24,spl[0]);
-			
-			var map=G.currentMap;
-			var spl2=spl[1].split(',');
-			var I=0;
-			for (var x=0;x<map.w;x++)
-			{
-				for (var y=0;y<map.h;y++)
-				{
-					if (spl2[I])
-					{
-						var tile=map.tiles[x][y];
-						spl3=spl2[I].split(':');
-						tile.owner=parseInt(spl3[0]);
-						tile.explored=parseInt(spl3[1])/100;
-					}
-					I++;
-				}
-			}
-			
-			G.updateMapForOwners(map);
-			G.centerMap(map);
-			
-			//techs & traits
-			var spl=str[s++].split(';');
-			//console.log('Techs : '+spl);
-			var len=spl.length;
-			for (var i=len-1;i>=0;i--)
-			{if (spl[i]!='') {G.gainTech(G.know[parseInt(spl[i])]);}}
-			
-			var spl=str[s++].split(';');
-			//console.log('Traits : '+spl);
-			var len=spl.length;
-			for (var i=len-1;i>=0;i--)
-			{if (spl[i]!='') G.gainTrait(G.know[parseInt(spl[i])]);}
-			
-			//policies
-			var spl=str[s++].split(';');
-			//console.log('Policies : '+spl);
-			var len=spl.length;
-			for (var i=len-1;i>=0;i--)
-			{if (spl[i]!='') {
-				var spl2=spl[i].split(',');
-				var me=G.policy[parseInt(spl2[0])];
-				G.gainPolicy(me);
-				me.mode=me.modesById[parseInt(spl2[1])];
-			}}
-			
-			//res
-			var spl=str[s++].split(';');
-			//console.log('Resources : '+spl);
-			var len=G.res.length;
-			for (var i=0;i<len;i++)
-			{
-				if (spl[i])
-				{
-					var me=G.res[i];
-					var spl2=spl[i].split(',');
-					if (parseInt(spl2[spl2.length-1])==1) me.visible=true; else me.visible=false;
-					if (!me.meta) me.amount=parseFloat(spl2[0]);
-					if (me.displayUsed) me.used=parseFloat(spl2[1]);
-				}
-			}
-			
-			//units
-			var spl=str[s++].split(';');
-			//console.log('Units : '+spl);
-			var len=spl.length;
-			for (var i=len-1;i>=0;i--)
-			{if (spl[i]!='')
-				{
-					var spl2=spl[i].split(',');
-					//unit id, amount, and if unit has gizmos : mode, percent
-					var obj={
-						id:G.unitN,
-						unit:G.unit[parseInt(spl2[0])],
-						amount:parseFloat(spl2[1]),
-						targetAmount:((typeof spl2[4]!=='undefined')?parseFloat(spl2[4]):parseFloat(spl2[1])),
-						idle:((typeof spl2[5]!=='undefined')?parseFloat(spl2[5]):0),
-						displayedAmount:0,
-						mode:parseInt(spl2[2])||0,
-						percent:parseInt(spl2[3]),
-						popups:[]
-						};
-					G.unitsOwned.unshift(obj);
-					var unit=G.unitsOwned[0];
-					if (unit.unit.modesById[0]) unit.mode=unit.unit.modesById[unit.mode];
-					G.unitsOwnedNames.unshift(G.unit[parseInt(spl2[0])].name);
-					G.unitN++;
-				}
-			}
-			
-			//assign unit .splitOf
-			var prev=0;
-			var len=G.unitsOwned.length;
-			for (var i=0;i<len;i++)
-			{
-				var me=G.unitsOwned[i];
-				if (prev && me.unit.id==prev.unit.id) me.splitOf=prev;
-				else prev=me;
-			}
-			prev=0;
-			
-			//chooseboxes
-			var spl=str[s++].split(';');
-			var len=spl.length;
-			for (var i=len-1;i>=0;i--)
-			{if (spl[i]!='')
-				{
-					G.chooseBox[i].choices=[];
-					var spl2=spl[i].split(',');
-					for (var ii in spl2)
-					{
-						if (ii==0) G.chooseBox[i].roll=parseFloat(spl2[ii]);
-						else G.chooseBox[i].choices[ii-1]=G.know[parseInt(spl2[ii])];
-					}
-				}
-			}
-			
-			G.runUnitReqs();
-			G.runPolicyReqs();
-			
-			G.applyAchievEffects('load');
-			
-			G.updateEverything();
-			G.createTopInterface();
-			G.createDebugMenu();
-			if (G.tabs[G.settingsByName['tab'].value]) G.setTab(G.tabs[G.settingsByName['tab'].value]);
-			G.setSetting('forcePaused',0);
-			
-			l('blackBackground').style.opacity=0;
-			if (timeOffline>=1) G.middleText('- Welcome back -<br><small>You accumulated '+B(timeOffline)+' fast ticks while you were away.</small>',true);
-			
-			G.rememberAchievs=true;
-			
-			G.animIntro=true;
-			G.introDur=G.fps*1;
-		
-			G.doFunc('game loaded');
-			
-			G.Logic(true);//force a tick (solves some issues with display updates; this howeverr means loading a paused game, saving and reloading will make a single day go by everytime, which isn't ideal)
-			
-			console.log('Game loaded successfully.');
-			return true;
-		}
-		return false;
-	}
+	
 	/*=====================================================================================
 	TECH & TRAIT CATEGORIES
 	=======================================================================================*/
@@ -19123,7 +18681,7 @@ new G.Tech({
 	});
 	new G.Trait({
         name:'patron1',
-	displayName:''+G.getName("patron")+' the Phoenix of Fire',
+	displayName:'Flamtnaja the Phoenix of Fire',
 	desc:'[patron1] represents flames and fires. @This patron also symbolizes fight and decisivness. @Your [Wizard]s and people picking this patron want to mark that their decisions are made with confidence. Also [Wizard]s believe that Phoenix will lead them to right paths of their lives. @<font color="fuschia">Patron does not involve gameplay in any way. It is lore thing.</font>',
         icon:[0,32,'magixmod'],
 	cost:{'culture':75,'faith':5},
@@ -19132,7 +18690,7 @@ new G.Tech({
     });
 	new G.Trait({
         name:'patron2',
-	displayName:''+G.getName("patron")+' The Son of the Forest',
+	displayName:'Uraloik The Son of the Forest',
 	desc:'[patron2] represents nature and its flora. @This patron is also symbolizes calm and peace. @Your [Wizard]s and people picking this patron want to mark that peace has keys to fix a lot of troubles and no agression is needed to solve every problem. Also [Wizard]s believe that The Son created once true Oasis of Peace. @<font color="fuschia">Patron does not involve gameplay in any way. It is lore thing.</font>',
         icon:[1,32,'magixmod'],
 	cost:{'culture':75,'faith':5},
@@ -19141,7 +18699,7 @@ new G.Tech({
     });
 	new G.Trait({
         name:'patron3',
-	displayName:''+G.getName("patron")+' The Guide of the Winds',
+	displayName:'Vikol The Guide of the Winds',
 	desc:'[patron3] represents winds and hurricanes. @This patron is also symbolizes time and impetuiosity. @Your [Wizard]s picking and sending their hopes to [patron3,Him] want to mark that each hard decision can be taken easily. Also [Wizard]s believe that one of [patron3,His] winds will blow their souls right to the Heaven preventing from being absorbed by darkness of Hell/Underworld. @<font color="fuschia">Patron does not involve gameplay in any way. It is lore thing.</font>',
         icon:[2,32,'magixmod'],
 	cost:{'culture':75,'faith':5},
@@ -19150,7 +18708,7 @@ new G.Tech({
     });
 	new G.Trait({
         name:'patron4',
-	displayName:''+G.getName("patron")+' the Dark Dragon',
+	displayName:'Selatarraya the Dark Dragon',
 	desc:'[patron4] represents mysteries , secrets and insecurity. @This patron also symbolizes withdrawal and concealment. @Your [Wizard]s picking this patron want to mark that their lives are full of secrets and mysteries. Also [Wizard]s believe that [patron4,The Dragon] will protect their secrets from being leaked and will curse "the leakers". Are there any miserables? @<font color="fuschia">Patron does not involve gameplay in any way. It is lore thing.</font>',
         icon:[3,32,'magixmod'],
 	cost:{'culture':75,'faith':5},
@@ -19159,7 +18717,7 @@ new G.Tech({
     });
 	new G.Trait({
         name:'patron5',
-	displayName:''+G.getName("patron")+' The Stormlord',
+	displayName:'Kerus The Stormlord',
 	desc:'[patron5] represents precision and impulsivity. @This patron also symbolizes indigation and quick action. @Your [Wizard]s picking this patron want to mark that sometimes all of us need to perform a decision very, very quickly. Also [Wizard]s believe that [patron5,The Stormlord] will guide through [population,people] lives helping making decisions with His precision. @<font color="fuschia">Patron does not involve gameplay in any way. It is lore thing.</font>',
         icon:[4,32,'magixmod'],
 	cost:{'culture':75,'faith':5},
@@ -19168,7 +18726,7 @@ new G.Tech({
     });
 	new G.Trait({
         name:'patron6',
-	displayName:''+G.getName("patron")+' ,Eye of the Sea',
+	displayName:'Olier\'ali ,Eye of the Sea',
 	desc:'[patron6] represents restraint and observation. @This patron also symbolizes cautioness and prudence. @Your [Wizard]s picking this patron want to mark that you cannot sometimes do something quickly... you just need to do it slowly and thoughtfully. Also [Wizard]s believe that [patron6,The Eye] will help [population,people] keep restraint and prevent obsession to appear in their hearts. @<font color="fuschia">Patron does not involve gameplay in any way. It is lore thing.</font>',
         icon:[5,32,'magixmod'],
 	cost:{'culture':75,'faith':5},
